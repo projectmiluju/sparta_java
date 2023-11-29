5f323eed9827df39730ae34f5bb8541b1beabe9b
projectmiluju <dnjsdyd0712@naver.com>
2023-11-29 23:12:00 +0900

    feat(프로그래머스):
    lv1. 최대공약수와 최소공배수

---
+++ src/main/java/MaxMin.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+//프로그랴머스 lv1 최대공약수와 최소공배수
+
+public class MaxMin {
+
+
+    public static void main(String[] args) {
+        int[] s = {1, 1, 1, 3, 2, 4, 4};
+        SameNumNo solution = new SameNumNo();
+        System.out.println(Arrays.toString(solution.solution(s)));
+    }
+
+}
