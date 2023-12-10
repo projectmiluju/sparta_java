cae47676cee5fa94cce7c76b25cbe774729459a9
projectmiluju <dnjsdyd0712@naver.com>
2023-12-10 12:44:46 +0900

    feat(프로그래머스): lv1 약수의 개수와 덧셈

---
+++ src/main/java/Algo1.java
@@ -0,0 +1,16 @@
+//프로그래머스 lv1 약수의 개수와 덧셈
+
+public class Algo1 {
+    public int solution(int left, int right){
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int a =  13;
+        int b = 17;
+
+        Algo1 solution = new Algo1();
+        System.out.println(solution.solution(a,b));
+    }
+}
