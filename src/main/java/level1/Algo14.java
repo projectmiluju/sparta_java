2fc4f04f95c34de911ab092cfca5f841a7058a1d
projectmiluju <dnjsdyd0712@naver.com>
2023-12-24 11:13:22 +0900

    feat(프로그래머스): lv1 콜라

--- src/main/java/level1/Algo14.java
+++ src/main/java/level1/Algo14.java
@@ -3,11 +3,21 @@ package level1;
 public class Algo14 {
     public int solution(int a, int b, int n) {
         int answer = 0;
+
+        while(true){
+            if (n<a){
+                break;
+            }
+
+            answer += (n/a)*b;
+
+            n = ((n/a)*b)+(n%a);
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         Algo14 algo = new Algo14();
-        System.out.println(algo.solution());
+        System.out.println(algo.solution(2,1,20));
     }
 }
