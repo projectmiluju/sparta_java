9dac33ea58af45e8c97456714ac733e6ee9e8679
projectmiluju <dnjsdyd0712@naver.com>
2023-12-31 13:38:01 +0900

    feat(프로그래머스): lv1 모의고사

---
+++ src/main/java/level1/Algo20.java
@@ -0,0 +1,16 @@
+package level1;
+
+import java.util.Arrays;
+
+public class Algo20 {
+    public int[] solution(int[] answers) {
+        int[] answer = new int[]{};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo20 algo = new Algo20();
+        int[] a = {1,2,3,4,5};
+        System.out.println(Arrays.toString(algo.solution(a)));
+    }
+}
