b591afbcdf999cda2d648f856e8570150bb0f9ce
projectmiluju <dnjsdyd0712@naver.com>
2024-01-01 14:00:40 +0900

    feat(프로그래머스): lv1 기사단원의 무기

---
+++ src/main/java/level1/Algo21.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class Algo21 {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo21 algo = new Algo21();
+        int a = 5;
+        int b = 3;
+        int c = 2;
+        System.out.println(algo.solution(a,b,c));
+    }
+}
