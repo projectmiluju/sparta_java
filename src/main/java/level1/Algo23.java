4397f80fc7d29bbecd2768a1a66bd08f1566ad26
projectmiluju <dnjsdyd0712@naver.com>
2024-01-03 13:39:30 +0900

    feat(프로그래머스): lv1 소수만들기

---
+++ src/main/java/level1/Algo23.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class Algo23 {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo23 algo = new Algo23();
+        System.out.println(algo.solution(10));
+        System.out.println(algo.solution(5));
+    }
+}
