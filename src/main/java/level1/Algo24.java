975c114f25d6ef25f9b912f844721971d62bd12f
projectmiluju <dnjsdyd0712@naver.com>
2024-01-04 06:33:50 +0900

    feat(프로그래머스): lv1 덧칠하기

---
+++ src/main/java/level1/Algo24.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class Algo24 {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo24 algo = new Algo24();
+        int n = 8;
+        int m = 4;
+        int[] section = {2, 3, 6};
+        System.out.println(algo.solution(n,m,section));
+    }
+}
