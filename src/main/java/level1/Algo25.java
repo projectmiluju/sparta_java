53a55051da790a893f0f5347e33e53b1e5642d1d
projectmiluju <dnjsdyd0712@naver.com>
2024-01-05 07:35:34 +0900

    feat(프로그래머스): lv1 실패율

---
+++ src/main/java/level1/Algo25.java
@@ -0,0 +1,17 @@
+package level1;
+
+import java.util.Arrays;
+
+public class Algo25 {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo25 algo = new Algo25();
+        int N = 5;
+        int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+        System.out.println(Arrays.toString(algo.solution(N, stages)));
+    }
+}
