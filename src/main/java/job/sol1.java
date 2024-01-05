8e69f637dc8312fb9ed3b35222079a72291ec2ff
projectmiluju <dnjsdyd0712@naver.com>
2024-01-05 10:39:16 +0900

    feat(정처기):
    ++x
    x를 먼저 1증가시킨 후 그 값을 사용
    --x
    x를 먼저 1감소시킨 후 그 값을 사용
    x++
    x값을 먼저 사용한 후 1
    증가
    x--
    x값을 먼저 사용한 후 1 감소

---
+++ src/main/java/job/sol1.java
@@ -0,0 +1,10 @@
+package job;
+
+public class sol1 {
+    public static void main(String[] args) {
+        int x = 5, y = 0, z =0;
+        y = x++;
+        z = --x;
+        System.out.println(x+","+y+","+z);
+    }
+}
