75de896f6c1c36789b645dd02eafcbf9119104e3
projectmiluju <dnjsdyd0712@naver.com>
2024-01-06 19:55:17 +0900

    refactor(프로그래머스): lv1 실패율
    해결못함

--- src/main/java/level1/Algo25_none.java
+++ src/main/java/level1/Algo25_none.java
@@ -1,15 +1,16 @@
 package level1;
 
 import java.util.Arrays;
+//실패율
 
-public class Algo25 {
+public class Algo25_none {
     public int[] solution(int N, int[] stages) {
         int[] answer = {};
         return answer;
     }
 
     public static void main(String[] args) {
-        Algo25 algo = new Algo25();
+        Algo25_none algo = new Algo25_none();
         int N = 5;
         int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
         System.out.println(Arrays.toString(algo.solution(N, stages)));
