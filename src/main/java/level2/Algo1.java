56d984900deb0e47f604606e7462bcc5229ce546
projectmiluju <dnjsdyd0712@naver.com>
2024-01-07 10:53:07 +0900

    feat(프로그래머스): lv2 최댓값과 최솟값

---
+++ src/main/java/level2/Algo1.java
@@ -0,0 +1,14 @@
+package level2;
+
+public class Algo1 {
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo1 algo = new Algo1();
+        String s = "1 2 3 4";
+        System.out.println(algo.solution(s));
+    }
+}
