9f6bbbb3961fe47e3c8f74da98af84fb954ee3e2
projectmiluju <dnjsdyd0712@naver.com>
2024-01-07 11:17:42 +0900

    feat(프로그래머스): lv2 최댓값과 최솟값

--- src/main/java/level2/Algo1.java
+++ src/main/java/level2/Algo1.java
@@ -1,14 +1,27 @@
 package level2;
 
+import java.util.Arrays;
+
 public class Algo1 {
     public String solution(String s) {
+        String[] str = s.split(" ");
+        int[] arr = new int[str.length];
+        for (int i = 0; i < str.length; i++){
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(arr);
+
         String answer = "";
+        answer += String.valueOf(arr[0]);
+        answer += " ";
+        answer += String.valueOf(arr[str.length-1]);
+
         return answer;
     }
 
     public static void main(String[] args) {
         Algo1 algo = new Algo1();
-        String s = "1 2 3 4";
+        String s = "-1 2 3 4";
         System.out.println(algo.solution(s));
     }
 }
