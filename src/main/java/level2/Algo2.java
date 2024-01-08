d01b70bc29288d84e034aff9c5ca62f1bba45d0b
projectmiluju <dnjsdyd0712@naver.com>
2024-01-08 13:31:11 +0900

    feat(프로그래머스): lv2 JadenCase 문자열만들기

---
+++ src/main/java/level2/Algo2.java
@@ -0,0 +1,14 @@
+package level2;
+
+public class Algo2 {
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo2 algo = new Algo2();
+        String s = "3people unFollowed me";
+        System.out.println(algo.solution(s));
+    }
+}
