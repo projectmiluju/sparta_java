f3a27cc693a21a34d03a255f05ace106a5bb9326
projectmiluju <dnjsdyd0712@naver.com>
2024-01-08 14:12:35 +0900

    feat(프로그래머스): lv2 JadenCase 문자열만들기
    런타임 오류
    일부 테스트 통과 실패

--- src/main/java/level2/Algo2.java
+++ src/main/java/level2/Algo2.java
@@ -3,12 +3,20 @@ package level2;
 public class Algo2 {
     public String solution(String s) {
         String answer = "";
+        String[] str = s.split(" ");
+        for (int i = 0; i < str.length; i++){
+            String firstLetter = str[i].substring(0,1).toUpperCase();
+            String remainLetter = str[i].substring(1).toLowerCase();
+            str[i] = firstLetter + remainLetter;
+            answer = answer + str[i] + " ";
+        }
+        answer = answer.substring(0, answer.length()-1);
         return answer;
     }
 
     public static void main(String[] args) {
         Algo2 algo = new Algo2();
-        String s = "3people unFollowed me";
+        String s = "for the last week";
         System.out.println(algo.solution(s));
     }
 }
