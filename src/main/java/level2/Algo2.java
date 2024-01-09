da9fb5313e7bceefc03b10534d4fb71cbd7cc27d
projectmiluju <dnjsdyd0712@naver.com>
2024-01-09 12:10:32 +0900

    fix(프로그래머스): lv2 JadenCase 문자열만들기
    경우의 수를 전부 고려하지 못했음.
    
    생각을 한번 더 하자

--- src/main/java/level2/Algo2.java
+++ src/main/java/level2/Algo2.java
@@ -3,20 +3,27 @@ package level2;
 public class Algo2 {
     public String solution(String s) {
         String answer = "";
+
         String[] str = s.split(" ");
+
         for (int i = 0; i < str.length; i++){
-            String firstLetter = str[i].substring(0,1).toUpperCase();
-            String remainLetter = str[i].substring(1).toLowerCase();
-            str[i] = firstLetter + remainLetter;
-            answer = answer + str[i] + " ";
+            if (str[i].isEmpty()){
+                answer += " ";
+            } else {
+                answer += str[i].substring(0,1).toUpperCase();
+                answer += str[i].substring(1).toLowerCase();
+                answer += " ";
+            }
         }
-        answer = answer.substring(0, answer.length()-1);
-        return answer;
+        if (s.endsWith(" ")){
+            return answer;
+        }
+        return answer.substring(0,answer.length()-1);
     }
 
     public static void main(String[] args) {
         Algo2 algo = new Algo2();
-        String s = "for the last week";
+        String s = " ";
         System.out.println(algo.solution(s));
     }
 }
