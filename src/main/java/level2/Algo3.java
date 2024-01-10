3e103881972c51facac9cb3977526a7cf0c901cc
heybobbiesinger <dnjsdyd0712@naver.com>
2024-01-10 11:34:21 +0900

    feat[프로그래머스]: lv2 최솟값 만들기

--- src/main/java/level2/Algo3.java
+++ src/main/java/level2/Algo3.java
@@ -1,8 +1,15 @@
 package level2;
 
+import java.util.Arrays;
+
 public class Algo3 {
     public int solution(int []A, int []B) {
         int answer = 0;
+        for (int i = 0; i<A.length; i++){
+            Arrays.sort(A);
+            Arrays.sort(B);
+            answer = answer + (A[i]*B[A.length-i-1]);
+        }
         return answer;
     }
 
