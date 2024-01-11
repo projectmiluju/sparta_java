8998a5089bfb55259c7f8ddb128f9acd5be4b329
projectmiluju <dnjsdyd0712@naver.com>
2024-01-11 14:51:44 +0900

    feat(프로그래머스): lv2 올바른괄호

---
+++ src/main/java/level2/Algo4.java
@@ -0,0 +1,15 @@
+package level2;
+
+public class Algo4 {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo4 algo = new Algo4;
+        String s = "()()";
+        System.out.println(algo.solution(s));
+    }
+}
