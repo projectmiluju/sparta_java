32c9c2fb45dcd563f663e1b85d3549ea9748bcc2
projectmiluju <dnjsdyd0712@naver.com>
2024-01-13 14:54:19 +0900

    feat(프로그래머스): lv2 올바른괄호
    stack에 대한 공부가 필요하다.

--- src/main/java/level2/Algo4.java
+++ src/main/java/level2/Algo4.java
@@ -1,14 +1,25 @@
 package level2;
 
+import java.util.Stack;
+
 public class Algo4 {
     boolean solution(String s) {
-        boolean answer = true;
-
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
-        Algo4 algo = new Algo4;
+        Algo4 algo = new Algo4();
         String s = "()()";
         System.out.println(algo.solution(s));
     }
