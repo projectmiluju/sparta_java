3272835ba764cb8b08282b09f754f4054151e35b
heybobbiesinger <dnjsdyd0712@naver.com>
2024-01-16 21:10:29 +0900

    feat[프로그래머스]: lv2 이진 변환 반복하기

---
+++ src/main/java/level2/Algo5.java
@@ -0,0 +1,20 @@
+package level2;
+
+import java.util.Arrays;
+
+public class Algo5 {
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo5 algo = new Algo5();
+        String s = "110010101001";
+        String t = "01110";
+        String r = "1111111";
+        System.out.println(Arrays.toString(algo.solution(s)));
+        System.out.println(Arrays.toString(algo.solution(t)));
+        System.out.println(Arrays.toString(algo.solution(r)));
+    }
+}
