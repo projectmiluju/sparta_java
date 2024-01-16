429fcbf6c527a401f3f82bfc3a977181a74d33ab
heybobbiesinger <dnjsdyd0712@naver.com>
2024-01-16 22:30:22 +0900

    feat[프로그래머스]: lv2 이진 변환 반복하기

--- src/main/java/level2/Algo5.java
+++ src/main/java/level2/Algo5.java
@@ -5,6 +5,15 @@ import java.util.Arrays;
 public class Algo5 {
     public int[] solution(String s) {
         int[] answer = {};
+        int zerosum = 0;
+        int zerocount = 0;
+        do {
+            zerosum = zerosum + s.length() - s.replace("0","").length();
+            s = s.replace("0","");
+            s = Integer.toBinaryString(s.length());
+            zerocount = zerocount + 1;
+            answer = new int[]{zerocount,zerosum};
+        } while (!s.equals("1"));
         return answer;
     }
 
@@ -14,7 +23,7 @@ public class Algo5 {
         String t = "01110";
         String r = "1111111";
         System.out.println(Arrays.toString(algo.solution(s)));
-        System.out.println(Arrays.toString(algo.solution(t)));
-        System.out.println(Arrays.toString(algo.solution(r)));
+//        System.out.println(Arrays.toString(algo.solution(t)));
+//        System.out.println(Arrays.toString(algo.solution(r)));
     }
 }
