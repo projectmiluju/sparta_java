2760c9888924794c063b3e647051a6efe5fc6f97
heybobbiesinger <dnjsdyd0712@naver.com>
2024-01-17 21:47:14 +0900

    feat[프로그래머스]: lv2 숫자의 표현

---
+++ src/main/java/level2/Algo6.java
@@ -0,0 +1,14 @@
+package level2;
+
+public class Algo6 {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo6 algo = new Algo6();
+        int n = 15;
+        System.out.println(algo.solution(n));
+    }
+}
