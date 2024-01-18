69b96c1ad843aef4aa987a6106d3b1fa6935b7fd
projectmiluju <dnjsdyd0712@naver.com>
2024-01-19 00:30:58 +0900

    feat(프로그래머스): lv2 피보나치 수
    이것은 지금 풀수 없을 듯 하다,,

---
+++ src/main/java/level2/Algo8_none.java
@@ -0,0 +1,16 @@
+package level2;
+
+public class Algo8_none {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo8_none algo = new Algo8_none();
+        int n = 3;
+        int m = 5;
+        System.out.println(algo.solution(n));
+        System.out.println(algo.solution(m));
+    }
+}
