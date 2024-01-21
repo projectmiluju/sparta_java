0f00c473c1eff3961b8fc79a3ec790d0c7ba5d37
projectmiluju <dnjsdyd0712@naver.com>
2024-01-21 18:18:23 +0900

    feat(프로그래머스): lv2 카펫

--- src/main/java/level2/Algo9.java
+++ src/main/java/level2/Algo9.java
@@ -7,12 +7,14 @@ public class Algo9 {
     public int solution(String s)
     {
         char[] s2 = s.toCharArray();
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();;
 
         for (int i = 0; i < s2.length; i++){
             char c = s2[i];
 
-            if (stack.isEmpty()) stack.push(c);
+            if (stack.isEmpty()) {
+                stack.push(c);
+            }
 
             else {
                 if (stack.peek() == c){
@@ -30,6 +32,6 @@ public class Algo9 {
         String s = "baabaa";
         String t = "cdcd";
         System.out.println(algo.solution(s));
-        System.out.println(t);
+//        System.out.println(algo.solution(t));
     }
 }
