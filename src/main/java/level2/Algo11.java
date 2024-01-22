9231c7843a5507a81276b1c98a7ff87e94c00181
projectmiluju <dnjsdyd0712@naver.com>
2024-01-22 13:31:43 +0900

    feat(프로그래머스): lv2 구명보트

--- src/main/java/level2/Algo11.java
+++ src/main/java/level2/Algo11.java
@@ -1,8 +1,18 @@
 package level2;
 
+import java.util.Arrays;
+
 public class Algo11 {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+        for (int i = people.length -1; i >= index; i--){
+            if (people[i] + people[index] <= limit ){
+                index ++;
+            }
+            answer ++;
+        }
         return answer;
     }
 
