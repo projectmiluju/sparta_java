47a36018eb1f7f56434220932182c1950fde3115
projectmiluju <dnjsdyd0712@naver.com>
2024-01-22 13:17:49 +0900

    feat(프로그래머스): lv2 구명보트

---
+++ src/main/java/level2/Algo11.java
@@ -0,0 +1,17 @@
+package level2;
+
+public class Algo11 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo11 algo = new Algo11();
+        int[] n = {70, 50, 80, 50};
+        int[] m = {70, 80, 50};
+        int limit = 100;
+        System.out.println(algo.solution(n, limit));
+        System.out.println(algo.solution(m, limit));
+    }
+}
