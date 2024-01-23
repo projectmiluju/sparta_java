7721bf15847683f9685a908789d9c55c35a8b870
projectmiluju <dnjsdyd0712@naver.com>
2024-01-23 19:51:34 +0900

    feat(프로그래머스): lv2 영어 끝말잇기

---
+++ src/main/java/level2/Algo12.java
@@ -0,0 +1,18 @@
+package level2;
+
+import java.util.Arrays;
+
+public class Algo12 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo12 algo = new Algo12();
+        int n = 3;
+        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+        System.out.println(Arrays.toString(algo.solution(n, words)));
+    }
+}
