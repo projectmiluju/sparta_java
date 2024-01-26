1293ef590d23d0985465d626ff0ca2b97bceabce
projectmiluju <dnjsdyd0712@naver.com>
2024-01-26 21:41:59 +0900

    feat(프로그래머스): lv2 영어 끝말잇기

--- src/main/java/level2/Algo12.java
+++ src/main/java/level2/Algo12.java
@@ -1,28 +1,28 @@
 package level2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.Arrays;
+    import java.util.List;
 
 public class Algo12 {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
         List<String> list = new ArrayList<>();
 
-        for(int i = 0; i<words.length; i++){
-            if (list.contains(words[i])){
-
+        for(int i = 0; i < words.length; i++){
+            if(list.contains(words[i])) {
                 answer[0] = (i % n) + 1;
                 answer[1] = (i / n) + 1;
                 break;
             }
             list.add(words[i]);
-
-            if (i > 0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
+            if(i > 0 && words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)){
                 answer[0] = (i % n) + 1;
                 answer[1] = (i / n) + 1;
+                break;
             }
         }
+
         return answer;
     }
 
