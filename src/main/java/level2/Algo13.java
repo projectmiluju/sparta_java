f649a117f57b5f5c5ffdc03b3f5b121fa267aaf7
projectmiluju <dnjsdyd0712@naver.com>
2024-01-27 19:12:59 +0900

    feat(프로그래머스): lv2 점프와 순간 이동

---
+++ src/main/java/level2/Algo13.java
@@ -0,0 +1,16 @@
+package level2;
+
+
+public class Algo13 {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo13 algo = new Algo13();
+        System.out.println(algo.solution(5));
+        System.out.println(algo.solution(6));
+        System.out.println(algo.solution(5000));
+    }
+}
