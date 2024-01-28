3fbc3ec9a76588cba513a7254bb0e9ee944ee38a
projectmiluju <dnjsdyd0712@naver.com>
2024-01-28 20:14:21 +0900

    feat(프로그래머스): lv2 점프와 순간 이동
    수학적 사고력을..키워야 하는것인가..

--- src/main/java/level2/Algo13.java
+++ src/main/java/level2/Algo13.java
@@ -4,6 +4,14 @@ package level2;
 public class Algo13 {
     public int solution(int n) {
         int answer = 0;
+        while (n != 0){
+            if (n % 2 == 0){
+                n /= 2;
+            }else {
+                n--;
+                answer++;
+            }
+        }
         return answer;
     }
 
