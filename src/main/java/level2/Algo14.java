5f0d43bebb99a6c161d796579f910ab928fb0ce9
projectmiluju <dnjsdyd0712@naver.com>
2024-01-29 20:22:29 +0900

    feat(프로그래머스): lv2 N개의 최소공배수

---
+++ src/main/java/level2/Algo14.java
@@ -0,0 +1,17 @@
+package level2;
+
+public class Algo14 {
+    public int solution(int[] arr) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo14 algo = new Algo14();
+        int[] arr1 = {2,6,8,14};
+        int[] arr2 = {1,2,3};
+        System.out.println(algo.solution(arr1));
+        System.out.println(algo.solution(arr2));
+
+    }
+}
