fab4fda717add41e46b9f0d2e3520f40556543d2
projectmiluju <dnjsdyd0712@naver.com>
2024-01-30 18:58:02 +0900

    feat(프로그래머스): lv2 N개의 최소공배수

--- src/main/java/level2/Algo14.java
+++ src/main/java/level2/Algo14.java
@@ -3,13 +3,32 @@ package level2;
 public class Algo14 {
     public int solution(int[] arr) {
         int answer = 0;
+
+        if (arr.length == 1) return arr[0];
+        int a = gcd(arr[0],arr[1]);
+        answer = (arr[0]*arr[1]) / a;
+
+        if (arr.length > 2){
+            for (int i = 2; i < arr.length; i++){
+                a = gcd(answer, arr[i]);
+                answer = (answer*arr[i]) / a;
+            }
+        }
+
         return answer;
+    }
+
+    //최대 공약수 구하기
+    private static int gcd(int a, int b){
+        int r = a % b;
+        if (r == 0) return b;
+        else return gcd(b,r);
     }
 
     public static void main(String[] args) {
         Algo14 algo = new Algo14();
         int[] arr1 = {2,6,8,14};
-        int[] arr2 = {1,2,3};
+        int[] arr2 = {2,5};
         System.out.println(algo.solution(arr1));
         System.out.println(algo.solution(arr2));
 
