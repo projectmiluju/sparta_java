b7d362f658791c2ce3c2ff1961bc978f2b39f11a
projectmiluju <dnjsdyd0712@naver.com>
2024-01-31 19:04:55 +0900

    feat(프로그래머스): lv3 정수 삼각형

---
+++ src/main/java/level3/Algo1.java
@@ -0,0 +1,14 @@
+package level3;
+
+public class Algo1 {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo1 algo = new Algo1;
+        int[][] triangle ={{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
+        System.out.println(algo.solution(triangle));
+    }
+}
