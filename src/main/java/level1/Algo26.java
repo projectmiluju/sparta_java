c2a05ac476b6c0efb6aba34e387c41636626f64a
projectmiluju <dnjsdyd0712@naver.com>
2024-02-01 23:40:59 +0900

    feat(프로그래머스): lv1 로또의 최고 순위와 최저 순위
    한개를 틀리는 이유는 무엇일까?
    맞긴 맞았는데 좀 더 클린하게 짜보자

--- src/main/java/level1/Algo26.java
+++ src/main/java/level1/Algo26.java
@@ -17,11 +17,20 @@ public class Algo26 {
                 zero ++;
             }
         }
-        if (num == 0){
+        // 전부다 0인 경우
+        if (zero == 6){
             answer = new int[]{1, 6};
-        } else if (num == 6) {
+        }
+        // 다 맞은 경우
+        else if (num == 6) {
             answer = new int[]{1, 1};
-        } else answer = new int[]{7-num-zero, 7-num};
+        }
+        // 다 틀린 경우
+        else if (num == 0) {
+            answer = new int[]{6,6};
+        }
+        // 그 외 의 경우
+        else answer = new int[]{7-num-zero, 7-num};
         return answer;
     }
 
@@ -30,14 +39,17 @@ public class Algo26 {
         int[] a1 = {44, 1, 0, 0, 31, 25};
         int[] a2 = {0, 0, 0, 0, 0, 0};
         int[] a3 = {45, 4, 35, 20, 3, 9};
+        int[] a4 = {7,8,9,10,11,12};
 
         int[] b1 = {31, 10, 45, 1, 6, 19};
         int[] b2 = {38, 19, 20, 40, 15, 25};
         int[] b3 = {20, 9, 3, 45, 4, 35};
+        int[] b4 = {1,2,3,4,5,6};
 
         System.out.println(Arrays.toString(algo.solution(a1, b1))); // {3,5}
         System.out.println(Arrays.toString(algo.solution(a2, b2))); // {1,6}
         System.out.println(Arrays.toString(algo.solution(a3, b3))); // {1,1}
+        System.out.println(Arrays.toString(algo.solution(a4, b4)));
 
     }
 }
