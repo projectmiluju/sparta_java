4ca083a7820701c430fccb96f815d8b0e1ec6ca2
projectmiluju <dnjsdyd0712@naver.com>
2024-02-04 03:22:19 +0900

    feat(프로그래머스): lv1 크레인 인형 뽑기
    list.getLast() -> list.get(list.size()-1)
    
    list.removeLast() ->
    list.remove(list.size()-1)

--- src/main/java/level1/Algo29.java
+++ src/main/java/level1/Algo29.java
@@ -1,8 +1,35 @@
 package level1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Algo29 {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < moves.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[j][moves[i]-1] != 0){
+                    if (list.isEmpty()){
+                        list.add(board[j][moves[i]-1]);
+                        board[j][moves[i]-1] = 0;
+                        break;
+//                    } else if (list.getLast() == board[j][moves[i]-1]) {
+                    } else if (list.get(list.size()-1) == board[j][moves[i]-1]) {
+//                        list.removeLast();
+                        list.remove(list.size()-1);
+                        answer ++;
+                        board[j][moves[i]-1] = 0;
+                        break;
+                    } else {
+                        list.add(board[j][moves[i]-1]);
+                        board[j][moves[i]-1] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        answer *=2;
         return answer;
     }
 
