0432afa46bbb6bcf650073dd19402c9dc03c4708
projectmiluju <dnjsdyd0712@naver.com>
2024-02-06 15:41:23 +0900

    feat(프로그래머스): lv1 신규 아이디 추천

---
+++ src/main/java/level1/Algo30.java
@@ -0,0 +1,30 @@
+package level1;
+
+public class Algo30 {
+    public String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo30 algo = new Algo30();
+
+        String id1 = "...!@BaT#*..y.abcdefghijklm";
+        System.out.println(algo.solution(id1));
+
+        String id2 = "z-+.^.";
+        System.out.println(algo.solution(id2));
+
+        String id3 = "=.=";
+        System.out.println(algo.solution(id3));
+
+        String id4 = "123_.def";
+        System.out.println(algo.solution(id4));
+
+        String id5 = "abcdefghijklmn.p";
+        System.out.println(algo.solution(id5));
+
+
+
+    }
+}
