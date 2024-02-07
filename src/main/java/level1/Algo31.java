320bb60171c6d8f6a73acd258a77987080e38236
projectmiluju <dnjsdyd0712@naver.com>
2024-02-07 17:48:31 +0900

    feat(프로그래머스): lv1 키패드 누르기

--- src/main/java/level1/Algo31.java
+++ src/main/java/level1/Algo31.java
@@ -1,5 +1,8 @@
 package level1;
 
+import java.util.Objects;
+import java.util.SortedMap;
+
 public class Algo31 {
     public String solution(int[] numbers, String hand) {
         String answer = "";
@@ -10,14 +13,20 @@ public class Algo31 {
         Algo31 algo = new Algo31();
         int[] n1 = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
         String hand1 = "right";
-        System.out.println(algo.solution(n1,hand1)); //
+        String result1 = "LRLLLRLLRRL";
+        System.out.println(algo.solution(n1,hand1));
+        if (Objects.equals(algo.solution(n1, hand1), result1)) System.out.println("Y"); else System.out.println("N");
 
         int[] n2 = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
         String hand2 = "left";
-        System.out.println(algo.solution(n2,hand2)); //
+        String result2 = "LRLLRRLLLRR";
+        System.out.println(algo.solution(n2,hand2));
+        if (Objects.equals(algo.solution(n2, hand2), result2)) System.out.println("Y"); else System.out.println("N");
 
         int[] n3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         String hand3 = "right";
-        System.out.println(algo.solution(n3,hand3)); //
+        String result3 = "LLRLLRLLRL";
+        System.out.println(algo.solution(n3,hand3));
+        if (Objects.equals(algo.solution(n3, hand3), result3)) System.out.println("Y"); else System.out.println("N");
     }
 }
