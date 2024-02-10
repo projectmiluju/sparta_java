48068cf4d70628cb7abbd52de9b058f04509e35b
projectmiluju <dnjsdyd0712@naver.com>
2024-02-10 10:00:09 +0900

    feat(프로그래머스): lv1 신고 결과 받기
    더 공부하고 다시 풀기

--- src/main/java/level1/Algo32_none.java
+++ src/main/java/level1/Algo32_none.java
@@ -1,15 +1,34 @@
 package level1;
 
 import java.util.Arrays;
+import java.util.Objects;
 
-public class Algo32 {
+
+public class Algo32_none {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = {};
+        String[][] str2  = new String[id_list.length][report.length];
+        for (int i = 0; i < id_list.length; i++){
+
+            int idx = 0;
+            for (int j = 0; j < report.length; j++){
+                String[] str1 = report[j].split(" ");
+                if (Objects.equals(id_list[i], str1[0])) {
+                    if (str2[i][idx].contains(str1[1])) {
+                    }else {
+                        str2[i][idx++] = str1[1];
+                    }
+                }
+            }
+
+        }
+        System.out.println(Arrays.deepToString(str2));
+
         return answer;
     }
 
     public static void main(String[] args) {
-        Algo32 algo = new Algo32();
+        Algo32_none algo = new Algo32_none();
 
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
         String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
