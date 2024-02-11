d5d0bbfa08c47f773b79ae99bcd045f0c252ee14
projectmiluju <dnjsdyd0712@naver.com>
2024-02-11 12:08:17 +0900

    feat(프로그래머스): lv1 성격 유형 검사하기

--- src/main/java/level1/Algo33.java
+++ src/main/java/level1/Algo33.java
@@ -3,6 +3,58 @@ package level1;
 public class Algo33 {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
+        int[][] sursum = new int[4][2];
+        for (int i = 0; i < choices.length; i++){
+            if ( choices[i] == 1) {
+                choices[i] = 3;
+            }else if ( choices[i] == 2) {
+                choices[i] = 2;
+            }else if ( choices[i] == 3) {
+                choices[i] = 1;
+            }else if ( choices[i] == 4){
+                choices[i] = 0;
+            } else if ( choices[i] == 5) {
+                choices[i] = -1;
+            } else if ( choices[i] == 6) {
+                choices[i] = -2;
+            } else if ( choices[i] == 7) {
+                choices[i] = -3;
+            }
+        }
+
+        for (int i = 0; i < survey.length; i++){
+            if (survey[i].charAt(0) == 'R') {
+                sursum[0][0] += choices[i];
+            } else if (survey[i].charAt(0) == 'T') {
+                sursum[0][1] += choices[i];
+            } else if (survey[i].charAt(0) == 'C') {
+                sursum[1][0] += choices[i];
+            } else if (survey[i].charAt(0) == 'F') {
+                sursum[1][1] += choices[i];
+            } else if (survey[i].charAt(0) == 'J') {
+                sursum[2][0] += choices[i];
+            } else if (survey[i].charAt(0) == 'M') {
+                sursum[2][1] += choices[i];
+            } else if (survey[i].charAt(0) == 'A') {
+                sursum[3][0] += choices[i];
+            } else if (survey[i].charAt(0) == 'N') {
+                sursum[3][1] += choices[i];
+            }
+        }
+
+        if (sursum[0][0] - sursum[0][1] >= 0) {
+            answer += "R";
+        } else answer += "T";
+        if (sursum[1][0] - sursum[1][1] >= 0) {
+            answer += "C";
+        } else answer += "F";
+        if (sursum[2][0] - sursum[2][1] >= 0) {
+            answer += "J";
+        } else answer += "M";
+        if (sursum[3][0] - sursum[3][1] >= 0) {
+            answer += "A";
+        } else answer += "N";
+
         return answer;
     }
 
