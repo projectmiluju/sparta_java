8a20beb49a79472a459729aa736a7602a1b415a2
projectmiluju <dnjsdyd0712@naver.com>
2024-02-12 22:12:00 +0900

    feat(프로그래머스): lv2 의상

---
+++ src/main/java/level2/Algo15.java
@@ -0,0 +1,24 @@
+package level2;
+
+public class Algo15 {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo15 algo = new Algo15();
+
+        String[][] clothes = {{"yellow_hat", "headgear"},{"blue_sunglasses", "eyewear"},{"blue_sunglasses", "eyewear"}};
+        int result = 5;
+        System.out.println(algo.solution(clothes));
+        if (algo.solution(clothes) == result) System.out.println("Y"); else System.out.println("N");
+
+        String[][] clothes2 = {{"crow_mask", "face"},{"blue_sunglasses", "face"},{"smoky_makeup", "face"}};
+        int result2 = 3;
+        System.out.println(algo.solution(clothes2));
+        if (algo.solution(clothes2) == result2) System.out.println("Y"); else System.out.println("N");
+
+    }
+
+}
