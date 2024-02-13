43085c6cb8d4d9554e7df4b186079cf2911aa9fd
projectmiluju <dnjsdyd0712@naver.com>
2024-02-13 22:36:12 +0900

    feat(함수 연습): hash

--- src/main/java/level2/Algo15.java
+++ src/main/java/level2/Algo15.java
@@ -1,8 +1,20 @@
 package level2;
 
+import java.util.HashMap;
+
 public class Algo15 {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
+        HashMap<String,Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        for(String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+        answer -= 1;
 
         return answer;
     }
