8a0f04ab2b7612d5a4f1c0931abf787648fb6047
projectmiluju <dnjsdyd0712@naver.com>
2024-02-14 23:08:04 +0900

    feat(프로그래머스): lv2 귤 고르기

--- src/main/java/level2/Algo16.java
+++ src/main/java/level2/Algo16.java
@@ -1,8 +1,31 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class Algo16 {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        int sum = 0;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num : tangerine){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        ArrayList<Integer> valueList = new ArrayList<>(map.values());
+        Collections.sort(valueList, Collections.reverseOrder());
+
+        for (int i : valueList){
+            if (sum + i >= k){
+                answer ++;
+                break;
+            } else {
+                sum += i;
+                answer++;
+            }
+        }
+
         return answer;
     }
 
@@ -15,7 +38,7 @@ public class Algo16 {
         System.out.println(algo.solution(k,tangerine));
         if (algo.solution(k,tangerine) == result) System.out.println("Y"); else System.out.println("N");
 
-        int k2 = 2;
+        int k2 = 4;
         int[] tangerine2 = {1, 3, 2, 5, 4, 5, 2, 3};
         int result2 = 2;
         System.out.println(algo.solution(k2,tangerine2));
