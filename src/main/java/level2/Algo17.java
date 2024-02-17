789a04d6811d87cfdb92d68965b15fe4744201c7
projectmiluju <dnjsdyd0712@naver.com>
2024-02-17 22:36:54 +0900

    feat(프로그래머스): lv2 타겟넘버
    깊이 우선 탐색 알고리즘

--- src/main/java/level2/Algo17.java
+++ src/main/java/level2/Algo17.java
@@ -1,24 +1,32 @@
 package level2;
 
 public class Algo17 {
+    int answer = 0;
+
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        dfs(numbers, 0, target, 0);
         return answer;
+    }
+
+    // 깊이 우선 탐색 알고리즘
+    public void dfs(int[] numbers, int depth, int target, int sum){
+        if(depth == numbers.length){
+            if(target == sum) answer++;
+        } else {
+            dfs(numbers, depth + 1, target, sum + numbers[depth]);
+            dfs(numbers, depth + 1, target, sum - numbers[depth]);
+        }
     }
 
     public static void main(String[] args) {
         Algo17 algo = new Algo17();
 
-        int[] numbers = {1, 1, 1, 1, 1};
-        int target = 3;
-        int result = 5;
+        int[] numbers = {4, 1, 2, 1};
+        int target = 4;
         System.out.println(algo.solution(numbers, target));
-        if (algo.solution(numbers, target) == result) System.out.println("Y"); else System.out.println("N");
+        int result = 2;
+        if (algo.answer == result) System.out.println("Y"); else System.out.println("N");
 
-        int[] numbers2 = {4, 1, 2, 1};
-        int target2 = 4;
-        int result2 = 2;
-        System.out.println(algo.solution(numbers2, target2));
-        if (algo.solution(numbers2, target2) == result2) System.out.println("Y"); else System.out.println("N");
+
     }
 }
