19ba7da6b33d10cb0d8c4f77aa82bd5461541918
projectmiluju <dnjsdyd0712@naver.com>
2024-02-19 06:27:08 +0900

    feat(프로그래머스): lv1 소수만들기

--- src/main/java/level1/Algo23.java
+++ src/main/java/level1/Algo23.java
@@ -2,8 +2,22 @@ package level1;
 
 public class Algo23 {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 1;
+        for (int i = 3; i <= n; i++){
+            if (isPrime(i)){
+                answer += 1;
+            }
+        }
         return answer;
+    }
+
+    private boolean isPrime(int num) {
+        for (int i = 2; i<= (int)Math.sqrt(num); i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
