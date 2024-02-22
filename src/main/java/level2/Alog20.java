5bd551f29f93db3e8c9a236185d0130e201c6575
projectmiluju <dnjsdyd0712@naver.com>
2024-02-22 15:29:28 +0900

    feat(프로그래머스): lv2 H-index

--- src/main/java/level2/Alog20.java
+++ src/main/java/level2/Alog20.java
@@ -1,16 +1,28 @@
 package level2;
 
+import java.util.Arrays;
+
 public class Alog20 {
     public int solution(int[] citations) {
         int answer = 0;
+        Arrays.sort(citations); // 0, 1, 4, 4, 5, 6, 7
+        for (int i = 0; i < citations.length; i++){
+            int n = citations.length -i;
+
+            if (citations[i] >= n){
+                answer = n;
+                break;
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         Alog20 algo = new Alog20();
 
-        int[] citations = {3, 0, 6, 1, 5};
-        int result = 3;
+        int[] citations = {4, 0, 6, 1, 5, 7, 4};
+//        int[] citations = {0,0,0};
+        int result = 4;
         System.out.println(algo.solution(citations));
         if (algo.solution(citations) == result) System.out.println("Y"); else System.out.println("N");
     }
