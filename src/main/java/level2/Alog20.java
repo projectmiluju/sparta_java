a67508683a6c6cb327919e37feb0395f5d11c0aa
projectmiluju <dnjsdyd0712@naver.com>
2024-02-22 13:34:04 +0900

    feat(프로그래머스): lv2 H-index

---
+++ src/main/java/level2/Alog20.java
@@ -0,0 +1,17 @@
+package level2;
+
+public class Alog20 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Alog20 algo = new Alog20();
+
+        int[] citations = {3, 0, 6, 1, 5};
+        int result = 3;
+        System.out.println(algo.solution(citations));
+        if (algo.solution(citations) == result) System.out.println("Y"); else System.out.println("N");
+    }
+}
