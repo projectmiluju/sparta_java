9f074589132c764524a48fb3dbf6ba49de1f2581
projectmiluju <dnjsdyd0712@naver.com>
2024-02-24 17:34:20 +0900

    feat(프로그래머스): lv2 가장 큰 수

--- src/main/java/level2/Algo22.java
+++ src/main/java/level2/Algo22.java
@@ -1,8 +1,27 @@
 package level2;
 
+import java.util.Arrays;
+
 public class Algo22 {
     public String solution(int[] numbers) {
+
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, ((o1, o2) -> (o2 + o1).compareTo(o1 + o2)));
+
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+
         String answer = "";
+
+        for (int i = 0; i < arr.length; i++){
+            answer += arr[i];
+        }
+
         return answer;
     }
 
