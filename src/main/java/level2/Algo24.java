f2418c51123a590431da0f7499778f5d3e512aa8
projectmiluju <dnjsdyd0712@naver.com>
2024-02-26 14:32:51 +0900

    feat(프로그래머스): lv2 주식가격

--- src/main/java/level2/Algo24.java
+++ src/main/java/level2/Algo24.java
@@ -4,7 +4,15 @@ import java.util.Arrays;
 
 public class Algo24 {
     public int[] solution(int[] prices) {
-        int[] answer = {};
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++){
+            for (int j = i + 1; j < prices.length; j++){
+                answer[i]++;
+                if (prices[i] > prices[j]){
+                    break;
+                }
+            }
+        }
 
         return answer;
     }
