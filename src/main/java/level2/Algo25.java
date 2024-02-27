3e5dfd8f50de74b989c563aaff303f483be3ec30
projectmiluju <dnjsdyd0712@naver.com>
2024-02-27 21:07:39 +0900

    feat(프로그래머스): lv2 더맵게

---
+++ src/main/java/level2/Algo25.java
@@ -0,0 +1,16 @@
+package level2;
+
+public class Algo25 {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo25 algo = new Algo25();
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+        System.out.println(algo.solution(scoville,k));
+    }
+
+}
