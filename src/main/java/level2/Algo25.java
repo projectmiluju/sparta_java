a08a394a35218b794355c33f6efee1e5834716ee
projectmiluju <dnjsdyd0712@naver.com>
2024-02-28 03:23:05 +0900

    feat(프로그래머스): lv2 더맵게

--- src/main/java/level2/Algo25.java
+++ src/main/java/level2/Algo25.java
@@ -1,8 +1,28 @@
 package level2;
 
+import java.util.PriorityQueue;
+
 public class Algo25 {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int i = 0; i < scoville.length; i++){
+            heap.add(scoville[i]);
+        }
+        int cnt = 0;
+
+        while (heap.peek() < K) {
+            if (heap.size() >= 2) {
+                int n = heap.poll() + heap.poll() * 2;
+                heap.add(n);
+                cnt++;
+            }else {
+                cnt = -1;
+                break;
+            }
+        }
+        System.out.println(heap);
+        answer = cnt;
         return answer;
     }
 
@@ -10,7 +30,7 @@ public class Algo25 {
         Algo25 algo = new Algo25();
         int[] scoville = {1, 2, 3, 9, 10, 12};
         int K = 7;
-        System.out.println(algo.solution(scoville,k));
+        System.out.println(algo.solution(scoville,K));
     }
 
 }
