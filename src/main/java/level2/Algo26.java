b265246a7342ea3a0453f0acff5542c05b9e6bcc
projectmiluju <dnjsdyd0712@naver.com>
2024-02-29 13:15:54 +0900

    feat(프로그래머스): lv2 124 나라의 숫자

---
+++ src/main/java/level2/Algo26.java
@@ -0,0 +1,18 @@
+package level2;
+
+public class Algo26 {
+    public String solution(int n) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo26 algo = new Algo26();
+        System.out.println(algo.solution(1));
+        System.out.println(algo.solution(2));
+        System.out.println(algo.solution(3));
+        System.out.println(algo.solution(4));
+
+    }
+}
