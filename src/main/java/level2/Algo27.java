7667f0cd2788d3f957470f445ccd738ed80c6a14
projectmiluju <dnjsdyd0712@naver.com>
2024-03-10 22:28:46 +0900

    fix(프로그래머스): lv2 문자열 압축

---
+++ src/main/java/level2/Algo27.java
@@ -0,0 +1,22 @@
+package level2;
+
+public class Algo27 {
+
+    public int solution(String s) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo27 algo = new Algo27();
+
+        System.out.println(algo.solution("aabbaccc")); //7
+        System.out.println(algo.solution("ababcdcdababcdcd")); //9
+        System.out.println(algo.solution("abcabcdede")); //8
+        System.out.println(algo.solution("abcabcabcabcdededededede")); //14
+        System.out.println(algo.solution("xababcdcdababcdcd")); //17
+
+
+    }
+}
