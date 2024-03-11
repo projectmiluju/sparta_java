532a124a86c9082e04f5127e1643cca0753cacaa
projectmiluju <dnjsdyd0712@naver.com>
2024-03-11 16:34:07 +0900

    fix(프로그래머스): lv2 문자열 압축

--- src/main/java/level2/Algo27.java
+++ src/main/java/level2/Algo27.java
@@ -3,7 +3,41 @@ package level2;
 public class Algo27 {
 
     public int solution(String s) {
-        int answer = 0;
+
+        int answer = Integer.MAX_VALUE;
+
+        if (s.length() == 1) return 1;
+
+        for (int i = 1; i <= s.length() / 2; i++){
+            StringBuffer sb = new StringBuffer();
+            String str = "";
+            int count = 1;
+
+            for (int j = 0; j <= s.length() - i; j += i){
+                String curStr = s.substring(j, j + i);
+
+                if (str.equals(curStr)){
+                    count++;
+                    continue;
+                }else if (count > 1){
+                    sb.append(count + str);
+                    count = 1;
+                }else {
+                    sb.append(str);
+                }
+
+                str = curStr;
+            }
+
+            sb.append(count > 1 ? count + str : str);
+
+            if (s.length() % i != 0){
+                sb.append(s.substring(s.length() - s.length() % i, s.length()));
+            }
+
+            answer = Math.min(answer,sb.length());
+
+        }
 
         return answer;
     }
