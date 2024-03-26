02303e5fe41d2b3dc67bfd2021a3b0095467eb45
projectmiluju <dnjsdyd0712@naver.com>
2024-03-26 18:30:49 +0900

    feat(프로그래머스): lv3 가장 먼 노드

--- src/main/java/level3/Algo3.java
+++ src/main/java/level3/Algo3.java
@@ -1,10 +1,58 @@
 package level3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Algo3 {
 
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        return answer;
+        ArrayList<Integer>[] adj = new ArrayList[n+1];
+        for (int i = 0; i < n+1; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < edge.length; i++) {
+            adj[edge[i][0]].add(edge[i][1]);
+            adj[edge[i][1]].add(edge[i][0]);
+        }
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+
+        boolean[] visited = new boolean[n+1];
+
+        for (int i = 0; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            int cur = 0;
+            for (int j = 1; j < n+1; j++) {
+                if (!visited[j] && dist[j] < min) {
+                    min = dist[j];
+                    cur = j;
+                }
+            }
+            visited[cur] = true;
+
+            for (int j = 0; j < adj[cur].size(); j++) {
+                int adjNode = adj[cur].get(j);
+
+                if(dist[adjNode] > dist[cur] + 1) {
+                    dist[adjNode] = dist[cur] + 1;
+                }
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int max = 0;
+        for (int i = 1; i < dist.length; i++) {
+            if(dist[i] != Integer.MAX_VALUE) {
+                max = Math.max(max, dist[i]);
+            }
+        }
+        for (int i = 1; i < dist.length; i++) {
+            if(dist[i] == max) {
+                list.add(i);
+            }
+        }
+        return list.size();
     }
 
     public static void main(String[] args) {
