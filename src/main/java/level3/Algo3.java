ba7dc75525fcd07d394d6680339d642744656250
projectmiluju <dnjsdyd0712@naver.com>
2024-03-26 17:47:01 +0900

    feat(프로그래머스): lv3 가장 먼 노드

---
+++ src/main/java/level3/Algo3.java
@@ -0,0 +1,17 @@
+package level3;
+
+public class Algo3 {
+
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo3 algo = new Algo3();
+        int n = 6;
+        int[][] vertex = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+
+        System.out.println(algo.solution(n, vertex));
+    }
+}
