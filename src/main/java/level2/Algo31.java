84c40146aaddf78feb75c4029d7f3bf0d42ef6c6
projectmiluju <dnjsdyd0712@naver.com>
2024-04-14 23:06:44 +0900

    docs(프로그래머스): lv2 캐시

--- src/main/java/level2/Algo31.java
+++ src/main/java/level2/Algo31.java
@@ -1,8 +1,8 @@
-package level1;
+package level2;
 
 import java.util.LinkedList;
 
-public class Algo35 {
+public class Algo31 {
     public int solution(int cacheSize, String[] cities) {
         if (cacheSize == 0) return cities.length*5;
 
@@ -25,42 +25,42 @@ public class Algo35 {
     }
 
     public static void main(String[] args) {
-        Algo35 algo35 = new Algo35();
+        Algo31 algo31 = new Algo31();
 
         int cacheSize1 = 3;
         String[] cities1 = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
         int result1 = 50;
-        System.out.println(algo35.solution(cacheSize1, cities1));
-        if (algo35.solution(cacheSize1, cities1) == result1) System.out.println("Y"); else System.out.println("N");
+        System.out.println(algo31.solution(cacheSize1, cities1));
+        if (algo31.solution(cacheSize1, cities1) == result1) System.out.println("Y"); else System.out.println("N");
 
         int cacheSize2 = 3;
         String[] cities2 = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
         int result2 = 21;
-        System.out.println(algo35.solution(cacheSize2, cities2));
-        if (algo35.solution(cacheSize2, cities2) == result2) System.out.println("Y"); else System.out.println("N");
+        System.out.println(algo31.solution(cacheSize2, cities2));
+        if (algo31.solution(cacheSize2, cities2) == result2) System.out.println("Y"); else System.out.println("N");
 
         int cacheSize3 = 2;
         String[] cities3 = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
         int result3 = 60;
-        System.out.println(algo35.solution(cacheSize3, cities3));
-        if (algo35.solution(cacheSize3, cities3) == result3) System.out.println("Y"); else System.out.println("N");
+        System.out.println(algo31.solution(cacheSize3, cities3));
+        if (algo31.solution(cacheSize3, cities3) == result3) System.out.println("Y"); else System.out.println("N");
 
         int cacheSize4 = 5;
         String[] cities4 = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
         int result4 = 52;
-        System.out.println(algo35.solution(cacheSize4, cities4));
-        if (algo35.solution(cacheSize4, cities4) == result4) System.out.println("Y"); else System.out.println("N");
+        System.out.println(algo31.solution(cacheSize4, cities4));
+        if (algo31.solution(cacheSize4, cities4) == result4) System.out.println("Y"); else System.out.println("N");
 
         int cacheSize5 = 2;
         String[] cities5 = {"Jeju", "Pangyo", "NewYork", "newyork"};
         int result5 = 16;
-        System.out.println(algo35.solution(cacheSize5, cities5));
-        if (algo35.solution(cacheSize5, cities5) == result5) System.out.println("Y"); else System.out.println("N");
+        System.out.println(algo31.solution(cacheSize5, cities5));
+        if (algo31.solution(cacheSize5, cities5) == result5) System.out.println("Y"); else System.out.println("N");
 
         int cacheSize6 = 0;
         String[] cities6 = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
         int result6 = 25;
-        System.out.println(algo35.solution(cacheSize6, cities6));
-        if (algo35.solution(cacheSize6, cities6) == result6) System.out.println("Y"); else System.out.println("N");
+        System.out.println(algo31.solution(cacheSize6, cities6));
+        if (algo31.solution(cacheSize6, cities6) == result6) System.out.println("Y"); else System.out.println("N");
     }
 }
