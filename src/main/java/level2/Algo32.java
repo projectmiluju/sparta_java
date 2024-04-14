589e025e1569eea29e6e093a0e7c338ddd348c31
projectmiluju <dnjsdyd0712@naver.com>
2024-04-14 23:09:04 +0900

    feat(프로그래머스): lv2 k진수에서 소수 개수 구하기

---
+++ src/main/java/level2/Algo32.java
@@ -0,0 +1,18 @@
+package level2;
+
+public class Algo32 {
+    public int solution(int n, int k) {
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo32 algo32 = new Algo32();
+
+        System.out.println(algo32.solution(437674,3));
+        if (algo32.solution(437674, 3) == 3) System.out.println("Y"); else System.out.println("N");
+
+        System.out.println(algo32.solution(110011, 10));
+        if (algo32.solution(110011, 10) == 2) System.out.println("Y"); else System.out.println("N");
+    }
+}
