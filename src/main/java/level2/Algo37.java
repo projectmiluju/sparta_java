6d31d1ed4a48c5cb5218bcca9576aaeab7eff06e
projectmiluju <dnjsdyd0712@naver.com>
2024-04-25 19:59:40 +0900

    feat(프로그래머스): lv2 파일명정렬

--- src/main/java/level2/Algo37.java
+++ src/main/java/level2/Algo37.java
@@ -1,11 +1,41 @@
 package level2;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Algo37 {
     public String[] solution(String[] files) {
         String[] answer = {};
-        return answer;
+
+        Arrays.sort(files, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                String head1 = o1.split("[0-9]")[0];
+                String head2 = o2.split("[0-9]")[0];
+
+                int result = head1.toLowerCase().compareTo(head2.toLowerCase());
+
+                if (result == 0) {
+                    result = convertNum(o1,head1) - convertNum(o2,head2);
+                }
+                return result;
+            }
+        });
+
+        return files;
+    }
+
+    public int convertNum(String str, String head) {
+        str = str.substring(head.length());
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i)) && result.length() < 5) {
+                result += str.charAt(i);
+            }else break;
+        }
+        return Integer.parseInt(result);
     }
 
     public static void main(String[] args) {
