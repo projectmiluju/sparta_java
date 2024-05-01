3893160b8adebb9a03f5c7f4328b9adbbd10c0eb
projectmiluju <dnjsdyd0712@naver.com>
2024-05-01 19:43:36 +0900

    feat(프로그래머스): lv2 메뉴 리뉴얼

--- src/main/java/level2/Algo43.java
+++ src/main/java/level2/Algo43.java
@@ -1,11 +1,57 @@
 package level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class Algo43 {
+    static HashMap<String, Integer> map;
     public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
+
+        for (int i = 0; i < orders.length; i++) {
+            char[] chars = orders[i].toCharArray();
+            Arrays.sort(chars);
+            orders[i] = String.valueOf(chars);
+        }
+
+        map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < course.length; i++) {
+            for (int j = 0; j < orders.length; j++) {
+                combination(orders[j], "", course[i]);
+            }
+            if (!map.isEmpty()){
+                ArrayList<Integer> maxList = new ArrayList<>(map.values());
+                int max = Collections.max(maxList);
+
+                if (max > 1){
+                    for (String key : map.keySet()){
+                        if (map.get(key) == max){
+                            list.add(key);
+                        }
+                    }
+                }
+            }
+            map.clear();
+        }
+        String[] answer = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        Arrays.sort(answer);
         return answer;
+    }
+
+    public void combination(String order, String com, int courseNum){
+        if (com.length() == courseNum){
+            map.put(com, map.getOrDefault(com, 0) + 1);
+            return;
+        }
+
+        for (int i = 0; i < order.length(); i++) {
+            combination(order.substring(i+1), com + order.charAt(i), courseNum );
+        }
     }
 
     public static void main(String[] args) {
