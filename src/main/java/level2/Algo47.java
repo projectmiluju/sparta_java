be23584bb4e9de1412e4657ffd3427fb8517e468
projectmiluju <dnjsdyd0712@naver.com>
2024-05-05 20:16:09 +0900

    feat(프로그래머스): lv2 택배 배달과 수거하기

--- src/main/java/level2/Algo47.java
+++ src/main/java/level2/Algo47.java
@@ -2,7 +2,28 @@ package level2;
 
 public class Algo47 {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = -1;
+        long answer = 0;
+
+//        long deliveriesSum = 0;
+//        long pickupsSum = 0;
+//        for (int i = 0; i < deliveries.length; i++) {
+//            deliveriesSum += deliveries[i];
+//            pickupsSum += pickups[i];
+//        }
+
+        int d = 0;
+        int p = 0;
+        for(int i=n-1; i>=0; i--){
+            d -= deliveries[i];
+            p -= pickups[i];
+
+            while(d < 0 || p < 0){
+                d += cap;
+                p += cap;
+                answer += (i+1)*2;
+            }
+        }
+
         return answer;
     }
 
@@ -14,15 +35,17 @@ public class Algo47 {
         int[] deliveries1 = {1, 0, 3, 1, 2};
         int[] pickups1 = {0, 3, 0, 4, 0};
         int res1 = 16;
-        System.out.println(algo47.solution(cap1, a, deliveries1, pickups1));
-        if (algo47.solution(cap1, a, deliveries1, pickups1) == res1) System.out.println("Y"); else System.out.println("N");
+        long ans1 = algo47.solution(cap1, a, deliveries1, pickups1);
+        System.out.println(ans1);
+        if (ans1 == res1) System.out.println("Y"); else System.out.println("N");
 
         int cap2 = 2;
         int b = 7;
         int[] deliveries2 = {1, 0, 2, 0, 1, 0, 2};
         int[] pickups2 = {0, 2, 0, 1, 0, 2, 0};
         int res2 = 30;
-        System.out.println(algo47.solution(cap2, b, deliveries2, pickups2));
-        if (algo47.solution(cap2, b, deliveries2, pickups2) == res2) System.out.println("Y"); else System.out.println("N");
+        long ans2 = algo47.solution(cap2, b, deliveries2, pickups2);
+        System.out.println(ans2);
+        if (ans2 == res2) System.out.println("Y"); else System.out.println("N");
     }
 }
