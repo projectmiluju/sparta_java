8ded35ba489943af55465edf53707a0594e14fa5
projectmiluju <dnjsdyd0712@naver.com>
2024-05-06 22:14:01 +0900

    feat(프로그래머스): lv2 택배 배달과 수거하기

--- src/main/java/level2/Algo48.java
+++ src/main/java/level2/Algo48.java
@@ -1,35 +1,59 @@
 package level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Algo48 {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = new int[2];
+        int left = 0;
+        int right = 0;
+        int partitionSum = sequence[0]; // 부분 수열의 합
 
-        int len = -1;
+        int n = sequence.length;
 
-        for (int i = 0; i < sequence.length; i++) {
-            int sum = 0;
-            for (int j = i; j < sequence.length; j++) {
-                sum += sequence[j];
-                if (k <= sum) {
-                    if (k == sum) {
+        List<SubSequence> subs = new ArrayList<>();
 
-                        if (len == -1){
-                            len = j-i;
-                            answer[0] = i;
-                            answer[1] = j;
-                        }else if (j-i < len) {
-                            len = j-i;
-                            answer[0] = i;
-                            answer[1] = j;
-                        }
-                    }
-                }
+        while (true){
+            // 현재 부분 수열의 합과 k가 일치하는 left와 right를 저장함
+            if(partitionSum == k){
+                subs.add(new SubSequence(left, right));
+            }
+            if(left == n && right == n) break;
+
+            if(partitionSum <= k && right < n){
+                right++;
+                // 새로운 원소가 추가되었으므로, right에 위치하는 값을 부분 수열 합에 더함
+                if(right < n) partitionSum += sequence[right];
+            } else {
+                // 기존의 left의 위치한 원소를 부분 수열의 합에서 제외
+                if(left < n) partitionSum -= sequence[left];
+                left++;
             }
         }
 
-        return answer;
+        // 조건에 가장 일치하는 부분 수열을 맨 앞으로 정렬
+        subs.sort(SubSequence::compareTo);
+
+        return new int[]{subs.get(0).left, subs.get(0).right};
+    }
+
+    private static class SubSequence implements Comparable<SubSequence>{
+        int left, right, size;
+
+        public SubSequence(int left, int right) {
+            this.left = left;
+            this.right = right;
+            this.size = right - left;
+        }
+
+        @Override
+        public int compareTo(SubSequence o) {
+            if(this.size == o.size){
+                return this.left - o.left;
+            }
+            return this.size - o.size;
+        }
     }
 
     public static void main(String[] args) {
