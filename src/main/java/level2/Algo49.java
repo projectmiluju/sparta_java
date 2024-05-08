ec2e9b238ad789b97d28a735386caa8e3ea5de07
projectmiluju <dnjsdyd0712@naver.com>
2024-05-08 15:56:48 +0900

    feat(프로그래머스): lv2 롤케이크 자르기
    실패 시간초과

--- src/main/java/level2/Algo49.java
+++ src/main/java/level2/Algo49.java
@@ -1,9 +1,38 @@
 package level2;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Algo49 {
     public int solution(int[] topping) {
-        int answer = -1;
-        return answer;
+
+        int cnt = 0;
+        for (int i = 1; i < topping.length; i++) {
+            int[] topA = Arrays.copyOfRange(topping, 0, i);
+            int[] topB = Arrays.copyOfRange(topping, i, topping.length);
+            if (cal(topA, topB)) {
+                cnt++;
+            }
+        }
+        if (cnt == 0){
+            return 0;
+        }
+        return cnt;
+    }
+
+    public boolean cal(int[] topA, int[] topB){
+        HashMap<Integer, Integer> mapA = new HashMap<>();
+        HashMap<Integer, Integer> mapB = new HashMap<>();
+        for (int i = 0; i < topA.length; i++) {
+            mapA.put(topA[i], mapA.getOrDefault(topA[i], 0) + 1);
+        }
+        for (int i = 0; i < topB.length; i++) {
+            mapB.put(topB[i], mapB.getOrDefault(topB[i], 0) + 1);
+        }
+        if (mapA.size() == mapB.size()){
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -15,7 +44,7 @@ public class Algo49 {
         System.out.println(ans1);
         if (res1 == ans1) System.out.println("Y"); else System.out.println("N");
 
-        int[] topping2 = {, 2, 3, 1, 4};
+        int[] topping2 = {1, 2, 3, 1, 4};
         int res2 = 0;
         int ans2 = algo49.solution(topping2);
         System.out.println(ans2);
