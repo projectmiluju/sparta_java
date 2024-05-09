84cdf466e5fc91c3816eb676e470bb79ec83fee2
projectmiluju <dnjsdyd0712@naver.com>
2024-05-10 08:01:38 +0900

    feat(프로그래머스): lv2 행렬의 곱셈

--- src/main/java/level2/Algo50.java
+++ src/main/java/level2/Algo50.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Algo50 {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1[0].length];
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
         for(int i = 0 ; i < arr1.length ; i++){
             for(int j = 0 ; j < arr2[0].length ; j++){
@@ -13,7 +13,6 @@ public class Algo50 {
                 }
             }
         }
-
         return answer;
     }
 
