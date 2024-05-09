b1589a103126fb1229e7e27fdd1fa543a0fe563f
projectmiluju <dnjsdyd0712@naver.com>
2024-05-10 07:31:03 +0900

    feat(프로그래머스): lv2 행렬의 곱셈

---
+++ src/main/java/level2/Algo50.java
@@ -0,0 +1,29 @@
+package level2;
+
+import java.util.Arrays;
+
+public class Algo50 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo50 algo50 = new Algo50();
+
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+        int[][] res1 = {{15, 15}, {15, 15}, {15, 15}};
+        int[][] ans1 = algo50.solution(arr1, arr2);
+        System.out.println(Arrays.deepToString(ans1));
+        if (Arrays.deepEquals(res1, ans1)) System.out.println("Y"); else System.out.println("N");
+
+        int[][] brr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
+        int[][] brr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+        int[][] res2 = {{22, 22, 11}, {36, 28, 18}, {29, 20, 14}};
+        int[][] ans2 = algo50.solution(brr1, brr2);
+        System.out.println(Arrays.deepToString(ans2));
+        if (Arrays.deepEquals(res2, ans2)) System.out.println("Y"); else System.out.println("N");
+    }
+}
