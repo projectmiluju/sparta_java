e92c0eba22f1c5c2b259bc28314b3c2b723e9332
projectmiluju <dnjsdyd0712@naver.com>
2024-05-09 14:36:01 +0900

    feat(프로그래머스): lv2 롤케이크 자르기
    실패 시간초과

--- src/main/java/level2/Algo49.java
+++ src/main/java/level2/Algo49.java
@@ -1,39 +1,34 @@
 package level2;
 
-import java.util.Arrays;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Algo49 {
     public int solution(int[] topping) {
+        int answer = 0;
 
-        int cnt = 0;
-        for (int i = 1; i < topping.length; i++) {
-            int[] topA = Arrays.copyOfRange(topping, 0, i);
-            int[] topB = Arrays.copyOfRange(topping, i, topping.length);
-            if (cal(topA, topB)) {
-                cnt++;
+        Map<Integer, Integer> mapA = new HashMap<>();
+        Map<Integer, Integer> mapB = new HashMap<>();
+
+        for (int i = 0; i < topping.length; i++) {
+            mapB.put(topping[i], mapB.getOrDefault(topping[i], 0) + 1);
+        }
+        for (int i = 0; i < topping.length; i++) {
+            mapA.put(topping[i], mapA.getOrDefault(topping[i], 0) + 1);
+
+            if (mapB.get(topping[i]) - 1 == 0) {
+                mapB.remove(topping[i]);
+            }else {
+                mapB.put(topping[i], mapB.get(topping[i]) - 1);
+            }
+            if (mapA.size() == mapB.size()) {
+                answer++;
             }
         }
-        if (cnt == 0){
-            return 0;
-        }
-        return cnt;
+        return answer;
     }
 
-    public boolean cal(int[] topA, int[] topB){
-        HashMap<Integer, Integer> mapA = new HashMap<>();
-        HashMap<Integer, Integer> mapB = new HashMap<>();
-        for (int i = 0; i < topA.length; i++) {
-            mapA.put(topA[i], mapA.getOrDefault(topA[i], 0) + 1);
-        }
-        for (int i = 0; i < topB.length; i++) {
-            mapB.put(topB[i], mapB.getOrDefault(topB[i], 0) + 1);
-        }
-        if (mapA.size() == mapB.size()){
-            return true;
-        }
-        return false;
-    }
 
     public static void main(String[] args) {
         Algo49 algo49 = new Algo49();
