bac6fdc99d7ccf5fe29491d0a9ef11ef699ac78e
projectmiluju <dnjsdyd0712@naver.com>
2024-05-11 08:09:03 +0900

    feat(프로그래머스): lv2 멀리 뛰기

---
+++ src/main/java/level2/Algo51.java
@@ -0,0 +1,24 @@
+package level2;
+
+public class Algo51 {
+    public long solution(int n) {
+        long answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo51 algo51 = new Algo51();
+
+        int a = 4;
+        long res1 = 5;
+        long ans1 = algo51.solution(a);
+        System.out.println(ans1);
+        if (res1 == ans1) System.out.println("Y"); else System.out.println("N");
+
+        int b = 3;
+        long res2 = 3;
+        long ans2 = algo51.solution(b);
+        System.out.println(ans2);
+        if (res2 == ans2) System.out.println("Y"); else System.out.println("N");
+    }
+}
