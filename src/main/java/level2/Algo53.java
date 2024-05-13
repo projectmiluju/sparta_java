692889d878d9723eee0348bd72a220907553952c
projectmiluju <dnjsdyd0712@naver.com>
2024-05-13 09:23:28 +0900

    feat(프로그래머스): lv2 스킬트리

--- src/main/java/level2/Algo53.java
+++ src/main/java/level2/Algo53.java
@@ -1,8 +1,22 @@
 package level2;
 
+
 public class Algo53 {
     public int solution(String skill, String[] skill_trees) {
-        int answer = -1;
+        int answer = 0;
+        for (int i = 0; i < skill_trees.length; i++) {
+            skill_trees[i] = skill_trees[i].replaceAll("[^"+skill+"]","");
+            boolean flag = false;
+            for (int j = 0; j < skill_trees[i].length(); j++) {
+                if (skill.charAt(j) != skill_trees[i].charAt(j)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == false){
+                answer++;
+            }
+        }
         return answer;
     }
 
