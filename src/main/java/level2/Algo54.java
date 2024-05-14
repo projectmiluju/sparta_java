0825a640c50dcdc3e0c30f42906a48806ddbd24d
projectmiluju <dnjsdyd0712@naver.com>
2024-05-14 09:39:31 +0900

    feat(프로그래머스): lv2 방문길이

---
+++ src/main/java/level2/Algo54.java
@@ -0,0 +1,24 @@
+package level2;
+
+public class Algo54 {
+    public int solution(String dirs) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo54 algo54 = new Algo54();
+
+        String dir1 = "ULURRDLLU";
+        int res1 = 7;
+        int ans1 = algo54.solution(dir1);
+        System.out.println(ans1);
+        if (ans1 == res1) System.out.println("Y"); else System.out.println("N");
+
+        String dir2 = "LULLLLLLU";
+        int res2 = 7;
+        int ans2 = algo54.solution(dir2);
+        System.out.println(ans2);
+        if (ans2 == res2) System.out.println("Y"); else System.out.println("N");
+    }
+}
