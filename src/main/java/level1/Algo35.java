4d060ea642a5e41e6fd25355ef8cd8b5664e0591
projectmiluju <dnjsdyd0712@naver.com>
2024-05-16 11:12:07 +0900

    feat(프로그래머스): lv1 대충 만든 자판
    일부 테스트 케이스 실패

--- src/main/java/level1/Algo35.java
+++ src/main/java/level1/Algo35.java
@@ -4,7 +4,20 @@ import java.util.Arrays;
 
 public class Algo35 {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = {};
+        int[] answer = new int[targets.length];
+        for (int i = 0; i < targets.length; i++) {
+            for (int j = 0; j < targets[i].length(); j++) {
+                int sum = -1;
+                for (int k = 0; k < keymap.length; k++) {
+                    if (keymap[k].contains(targets[i].charAt(j) + "")) {
+                        if (sum == -1 || sum > keymap[k].indexOf(targets[i].charAt(j) + "") + 1) {
+                            sum = keymap[k].indexOf(targets[i].charAt(j) + "") + 1;
+                        }
+                    }
+                }
+                answer[i] += sum;
+            }
+        }
         return answer;
     }
 
@@ -12,24 +25,24 @@ public class Algo35 {
         Algo35 algo35 = new Algo35();
 
         String[] keymap1 = {"ABACD", "BCEFD"};
-        String[] targets1 = {"ABCD","AABB"};
+        String[] targets1 = {"ABCD", "AABB"};
         int[] res1 = {9, 4};
         int[] answer1 = algo35.solution(keymap1, targets1);
         System.out.println(Arrays.toString(answer1));
-        if (answer1 == res1) System.out.println("Y"); else System.out.println("N");
+        if (Arrays.equals(res1,answer1)) System.out.println("Y"); else System.out.println("N");
 
         String[] keymap2 = {"AA"};
         String[] targets2 = {"B"};
         int[] res2 = {-1};
         int[] answer2 = algo35.solution(keymap2, targets2);
         System.out.println(Arrays.toString(answer2));
-        if (answer2 == res2) System.out.println("Y"); else System.out.println("N");
-
+        if (Arrays.equals(res2,answer2)) System.out.println("Y"); else System.out.println("N");
+//
         String[] keymap3 = {"AGZ", "BSSS"};
         String[] targets3 = {"ASA","BGZ"};
         int[] res3 = {4, 6};
         int[] answer3 = algo35.solution(keymap3, targets3);
         System.out.println(Arrays.toString(answer3));
-        if (answer3 == res3) System.out.println("Y"); else System.out.println("N");
+        if (Arrays.equals(res3,answer3)) System.out.println("Y"); else System.out.println("N");
     }
 }
