0c5294c0fe787aec89436eed774562fa114b6709
projectmiluju <dnjsdyd0712@naver.com>
2024-05-17 20:33:29 +0900

    feat(프로그래머스): lv1 둘만의 암호

--- src/main/java/level1/Algo36.java
+++ src/main/java/level1/Algo36.java
@@ -1,9 +1,28 @@
 package level1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Algo36 {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        String[] arr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        List<String> arrList = new ArrayList<>(Arrays.asList(arr));
+        for (int i = 0; i < skip.length(); i++){
+            arrList.remove(skip.substring(i, i + 1));
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            for (int j = 0; j < arrList.size(); j++){
+                if (arrList.get(j).equals(s.substring(i, i + 1))){
+                    int cnt = (j+index) % arrList.size();
+                    answer.append(arrList.get(cnt));
+                }
+
+            }
+        }
+        return answer.toString();
     }
 
     public static void main(String[] args) {
