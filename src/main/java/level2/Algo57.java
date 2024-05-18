20b4e15cc854b28bc63f8ff4584407d4a46f9130
projectmiluju <dnjsdyd0712@naver.com>
2024-05-19 02:04:19 +0900

    feat(프로그래머스): lv2 연속 부분 수열 합의 개수

--- src/main/java/level2/Algo57.java
+++ src/main/java/level2/Algo57.java
@@ -1,14 +1,38 @@
 package level2;
 
+import java.util.HashMap;
+
 public class Algo57 {
+
+    static HashMap<Integer,Integer> map;
     public int solution(int[] elements) {
         int answer = 0;
-        
         // 증복 제거 원소 개수 총합 구하기
         // elements의 길이만큼
+        map = new HashMap<>();
+        for (int i = 0; i < elements.length; i++){
+            int n = i+1;
+            elementSum(n,elements);
+        }
+        answer = map.size();
         return answer;
-
     }
+
+    private void elementSum(int n, int[] elements){
+
+        int[] arr = new int[elements.length+elements.length];
+        System.arraycopy(elements,0,arr,0,elements.length);
+        System.arraycopy(elements, 0,arr, elements.length, elements.length);
+
+        for (int i = 0; i < elements.length; i++){
+            int sum = 0;
+            for (int j = i; j < n + i; j++){
+                sum += arr[j];
+            }
+            map.put(sum,1);
+        }
+    }
+
 
     public static void main(String[] args) {
         Algo57 algo57 = new Algo57();
