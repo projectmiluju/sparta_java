a5e416320b211d935185efe75c69831dc3597f20
projectmiluju <dnjsdyd0712@naver.com>
2024-05-18 22:09:50 +0900

    feat(프로그래머스): lv2 쿼드압축 후 세기

--- src/main/java/level2/Algo56.java
+++ src/main/java/level2/Algo56.java
@@ -3,9 +3,36 @@ package level2;
 import java.util.Arrays;
 
 public class Algo56 {
+
+    static int[] answer = new int[2];
+
     public int[] solution(int[][] arr) {
-        int[] answer = {};
+        int totalSize = arr.length;
+        dp(0,0,totalSize,arr);
+
         return answer;
+    }
+
+    private static boolean check(int x, int y, int size, int[][] arr){
+        for (int i = x; i < x + size; i++){
+            for (int j = y; j < y + size; j++){
+                if (arr[x][y] != arr[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void dp(int startX, int startY, int size, int[][] arr){
+        if (check(startX, startY, size, arr)){
+            answer[arr[startX][startY]]++;
+            return;
+        }
+        dp(startX, startY, size / 2, arr);
+        dp(startX + size / 2, startY, size / 2, arr);
+        dp(startX, startY + size / 2, size / 2, arr);
+        dp(startX + size / 2, startY + size / 2, size / 2, arr);
     }
 
     public static void main(String[] args) {
@@ -18,7 +45,7 @@ public class Algo56 {
         if (Arrays.equals(res1, ans1)) System.out.println("Y"); else System.out.println("N");
 
         int[][] arr2 = {{1,1,1,1,1,1,1,1}, {0,1,1,1,1,1,1,1}, {0,0,0,0,1,1,1,1}, {0,1,0,0,1,1,1,1}, {0,0,0,0,0,0,1,1},{0,0,0,0,0,0,0,1}, {0,0,0,0,1,0,0,1}, {0,0,0,0,1,1,1,1}};
-        int[] res2 = {4, 9};
+        int[] res2 = {10, 15};
         int[] ans2 = algo56.solution(arr2);
         System.out.println(Arrays.toString(ans2));
         if (Arrays.equals(res2, ans2)) System.out.println("Y"); else System.out.println("N");
