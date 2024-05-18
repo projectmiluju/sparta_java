bf3b8407adefb9370ccb98478510ad3aa44215d9
projectmiluju <dnjsdyd0712@naver.com>
2024-05-18 22:12:38 +0900

    feat(프로그래머스): lv2 연속 부분 수열 합의 개수

---
+++ src/main/java/level2/Algo57.java
@@ -0,0 +1,18 @@
+package level2;
+
+public class Algo57 {
+    public int solution(int[] elements) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo57 algo57 = new Algo57();
+
+        int[] elements = new int[]{7,9,1,1,4};
+        int result = 18;
+        int answer = algo57.solution(elements);
+        System.out.println(answer);
+        if (result == answer) System.out.println("Y"); else System.out.println("N");
+    }
+}
