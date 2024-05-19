fdffa2cb7804072333ce863c7e5983ebeefb36a5
projectmiluju <dnjsdyd0712@naver.com>
2024-05-20 08:57:58 +0900

    feat(프로그래머스): lv2 전력망을 둘로 나누기

--- src/main/java/level2/Algo58.java
+++ src/main/java/level2/Algo58.java
@@ -1,9 +1,55 @@
 package level2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Algo58 {
+
+    static int[][] graph;
+
     public int solution(int n, int[][] wires) {
-        int answer = -1;
+        int answer = n;
+        graph = new int[n+1][n+1];
+
+        for (int i = 0; i < wires.length; i++) {
+            int from = wires[i][0];
+            int to = wires[i][1];
+            graph[from][to] = 1;
+            graph[to][from] = 1;
+        }
+        for (int i = 0; i < wires.length; i++) {
+            int from = wires[i][0];
+            int to = wires[i][1];
+            graph[from][to] = 0;
+            graph[to][from] = 0;
+
+            answer = Math.min(answer, bfs(n, from));
+            graph[from][to] = 1;
+            graph[to][from] = 1;
+        }
         return answer;
+    }
+
+    public static int bfs(int n, int x) {
+        boolean[] visited = new boolean[n+1];
+        int count = 1;
+
+        Queue<Integer> queue = new LinkedList<>();
+        visited[x] = true;
+        queue.offer(x);
+
+        while (!queue.isEmpty()) {
+            int temp = queue.poll();
+
+            for (int i = 1; i <= n; i++) {
+                if (graph[temp][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.offer(i);
+                    count ++;
+                }
+            }
+        }
+        return (int)Math.abs(count-(n-count));
     }
 
     public static void main(String[] args) {
