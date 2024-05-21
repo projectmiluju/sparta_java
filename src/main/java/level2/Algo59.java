b61ca05973a8f316e71277dc95704b6a3b137218
projectmiluju <dnjsdyd0712@naver.com>
2024-05-21 19:20:24 +0900

    feat(프로그래머스): lv2 마법의 엘레베이터

---
+++ src/main/java/level2/Algo59.java
@@ -0,0 +1,25 @@
+package level2;
+
+public class Algo59 {
+
+    public int solution(int storey) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo59 algo59 = new Algo59();
+
+        int storey1 = 16;
+        int res1 = 6;
+        int ans1 = algo59.solution(storey1);
+        System.out.println(ans1);
+        if (res1 == ans1) System.out.println("Y"); else System.out.println("N");
+
+        int storey2 = 2554;
+        int res2 = 16;
+        int ans2 = algo59.solution(storey2);
+        System.out.println(ans2);
+        if (res2 == ans2) System.out.println("Y"); else System.out.println("N");
+    }
+}
