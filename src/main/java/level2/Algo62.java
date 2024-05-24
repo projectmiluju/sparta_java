5f41a1fc1f71bb3dc1b9902364c9480f17848541
projectmiluju <dnjsdyd0712@naver.com>
2024-05-25 07:07:16 +0900

    feat(프로그래머스): lv2 시소 짝꿍

---
+++ src/main/java/level2/Algo62.java
@@ -0,0 +1,18 @@
+package level2;
+
+public class Algo62 {
+    public long solution(int[] weights) {
+        long answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo62 algo62 = new Algo62();
+
+        int[] weights = {100,180,360,100,270};
+        long result = 4;
+        long answer = algo62.solution(weights);
+        System.out.println(answer);
+        if (result == answer) System.out.println("Y"); else System.out.println("N");
+    }
+}
