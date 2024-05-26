cbc9956c3b8b8ef282ac87549127c248e051f2a9
projectmiluju <dnjsdyd0712@naver.com>
2024-05-27 01:26:37 +0900

    feat(프로그래머스): lv2 숫자 변환하기

--- src/main/java/level2/Algo64.java
+++ src/main/java/level2/Algo64.java
@@ -2,9 +2,31 @@ package level2;
 
 public class Algo64 {
     public int solution(int x, int y, int n) {
-        int answer = 0;
-        return answer;
+        int[] dp = new int[y + 1];
+
+        for (int i = x; i <= y; i++){
+            if (i != x && dp[i] == 0){
+                dp[i] = -1;
+                continue;
+            }
+
+            if (i + n <= y){
+                dp[i+n] = (dp[i+n] == 0) ? dp[i] +1 : Math.min(dp[i] + 1, dp[i+n]);
+            }
+
+            if (i * 2 <= y){
+                dp[i*2] = (dp[i*2] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i*2]);
+            }
+
+            if (i * 3 <= y){
+                dp[i*3] = (dp[i*3] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i*3]);
+            }
+
+
+        }
+        return dp[y];
     }
+
 
     public static void main(String[] args) {
         Algo64 algo64 = new Algo64();
