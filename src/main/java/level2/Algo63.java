28133c18612edc96576598bb089cb1c13e49877a
projectmiluju <dnjsdyd0712@naver.com>
2024-05-26 22:17:16 +0900

    feat(프로그래머스): lv2 테이블 해시 함수

--- src/main/java/level2/Algo63.java
+++ src/main/java/level2/Algo63.java
@@ -1,8 +1,26 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Algo63 {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
+
+        Arrays.sort(data, (o1, o2) -> o1[col-1] != o2[col-1] ? o1[col-1] - o2[col-1] : o2[0] - o1[0]);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = row_begin-1; i < row_end; i++){
+            int sum = 0;
+            for (int j = 0; j < data[0].length; j++){
+                sum += data[i][j] % (i+1);
+            }
+            list.add(sum);
+        }
+        answer = list.get(list.get(0));
+        for (int i = 1; i < list.size(); i++){
+            answer ^= list.get(i);
+        }
         return answer;
     }
 
