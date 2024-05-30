c75cba03bbc0db2b8d7101d369c21d6b755178ab
projectmiluju <dnjsdyd0712@naver.com>
2024-05-30 16:01:14 +0900

    feat(프로그래머스): lv3 숫자 게임

--- src/main/java/level3/Algo6.java
+++ src/main/java/level3/Algo6.java
@@ -1,9 +1,27 @@
 package level3;
 
+import java.util.Arrays;
+
 public class Algo6 {
     public int solution(int[] A, int[] B) {
-        int answer = -1;
-        return answer;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int aPoint = 0;
+        int bPoint = 0;
+        int result = 0;
+        for (int i = 0; i < A.length; i++){
+            if (A[aPoint] > B[bPoint]){
+                bPoint++;
+            } else if (A[aPoint] == B[bPoint]){
+                bPoint++;
+            } else {
+                aPoint++;
+                bPoint++;
+                result++;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
