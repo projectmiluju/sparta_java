509d4454db87bd4914f1fef78c93cd980a1089ca
projectmiluju <dnjsdyd0712@naver.com>
2024-06-01 08:36:29 +0900

    feat(프로그래머스): lv2 2개 이하로 다른 비트

--- src/main/java/level2/Algo65.java
+++ src/main/java/level2/Algo65.java
@@ -3,40 +3,52 @@ package level2;
 import java.util.Arrays;
 
 public class Algo65 {
-    public long[] solution(long[] numbers) {
-        long[] answer = new long[numbers.length];
+//    public long[] solution(long[] numbers) {
+//        long[] answer = new long[numbers.length];
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 2 == 0) {
+//                answer[i] = numbers[i]+1;
+//            }else {
+//                answer[i] = bitCal(numbers[i]);
+//
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    public static long bitCal(long n){
+//        long answer = 0;
+//        StringBuilder a = new StringBuilder(Long.toBinaryString(n));
+//        for (long i = n+1;;i++){
+//            String b = Long.toBinaryString(i);
+//            if (b.length() != a.length()) {
+//                a.insert(0, "0");
+//            }
+//            int cnt = 0;
+//            for (int j = 0; j < a.length(); j++) {
+//                if (a.charAt(j) != b.charAt(j)) {
+//                    cnt++;
+//                }
+//                if (cnt > 2){
+//                    break;
+//                }
+//            }
+//            if (cnt <= 2){
+//                answer = i;
+//                break;
+//            }
+//        }
+//        return answer;
+//    }
+
+    public static long[] solution(long[] numbers) {
+        long[] answer = numbers.clone();
         for (int i = 0; i < numbers.length; i++) {
-            answer[i] = bitCal(numbers[i]);
+            answer[i]++; // x보다 큰수로 만든다.
+            answer[i] += (answer[i] ^ numbers[i]) >> 2;
         }
         return answer;
     }
-
-    public static long bitCal(long n){
-        long answer = 0;
-        String a = Long.toBinaryString(n);
-        long b = n + 1;
-        while (answer == 0){
-            String bitB = Long.toBinaryString(b);
-            if (bitB.length() != a.length()){
-                a = "0"+a;
-            }
-            int cnt = 0;
-            for (int i = 0; i < a.length(); i++){
-                if (a.charAt(i) != bitB.charAt(i)){
-                    cnt++;
-                }
-                if (cnt > 2){
-                    b++;
-                    break;
-                }
-            }
-            if (cnt <= 2){
-                answer = b;
-            }
-        }
-        return answer;
-    }
-
     public static void main(String[] args) {
         Algo65 algo = new Algo65();
 
