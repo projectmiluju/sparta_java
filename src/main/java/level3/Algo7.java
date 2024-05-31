a34018402e7e60f39575f7781d8615f120ff1c31
projectmiluju <dnjsdyd0712@naver.com>
2024-05-31 12:46:55 +0900

    feat(프로그래머스): lv3 기지국 설치

--- src/main/java/level3/Algo7.java
+++ src/main/java/level3/Algo7.java
@@ -2,8 +2,18 @@ package level3;
 
 public class Algo7 {
     public int solution(int n, int[] stations, int w) {
-        int answer = 0;
+        int answer = 0, sIdx = 0, i = 1;
 
+        while (i <= n) {
+            if (sIdx < stations.length && i >= stations[sIdx] - w) {
+                i = stations[sIdx] + w + 1;
+                sIdx++;
+                continue;
+            }
+
+            i += 2 * w + 1;
+            answer++;
+        }
 
         return answer;
     }
