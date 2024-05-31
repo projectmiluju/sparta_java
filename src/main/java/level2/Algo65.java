f83d2fa56a129826d0fd70b04f44914832be8edf
projectmiluju <dnjsdyd0712@naver.com>
2024-06-01 06:31:22 +0900

    feat(프로그래머스): lv2 2개 이하로 다른 비트
    케이스 10, 11 시간초과

--- src/main/java/level2/Algo65.java
+++ src/main/java/level2/Algo65.java
@@ -4,7 +4,36 @@ import java.util.Arrays;
 
 public class Algo65 {
     public long[] solution(long[] numbers) {
-        long[] answer = {};
+        long[] answer = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = bitCal(numbers[i]);
+        }
+        return answer;
+    }
+
+    public static long bitCal(long n){
+        long answer = 0;
+        String a = Long.toBinaryString(n);
+        long b = n + 1;
+        while (answer == 0){
+            String bitB = Long.toBinaryString(b);
+            if (bitB.length() != a.length()){
+                a = "0"+a;
+            }
+            int cnt = 0;
+            for (int i = 0; i < a.length(); i++){
+                if (a.charAt(i) != bitB.charAt(i)){
+                    cnt++;
+                }
+                if (cnt > 2){
+                    b++;
+                    break;
+                }
+            }
+            if (cnt <= 2){
+                answer = b;
+            }
+        }
         return answer;
     }
 
