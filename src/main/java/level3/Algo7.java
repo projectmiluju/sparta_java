13a358f2bd56bf0be385556326c87b0a1428f40b
projectmiluju <dnjsdyd0712@naver.com>
2024-05-31 11:29:59 +0900

    feat(프로그래머스): lv3 기지국 설치

---
+++ src/main/java/level3/Algo7.java
@@ -0,0 +1,26 @@
+package level3;
+
+public class Algo7 {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo7 algo7 = new Algo7();
+
+        int N1 = 11, W1 = 1, res1 = 3;
+        int[] stations1 = {4 ,11};
+        int ans1 = algo7.solution(N1, stations1, W1);
+        System.out.println(ans1);
+        if (ans1 == res1) System.out.println("Y"); else System.out.println("N");
+
+        int N2 = 16, W2 = 2, res2 = 3;
+        int[] stations2 = {9};
+        int ans2 = algo7.solution(N2, stations2, W2);
+        System.out.println(ans2);
+        if (ans2 == res2) System.out.println("Y"); else System.out.println("N");
+    }
+}
