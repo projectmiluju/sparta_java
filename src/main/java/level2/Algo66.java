d6030bd989cedeba4ff1a27e54fe210ea6e155ef
projectmiluju <dnjsdyd0712@naver.com>
2024-06-03 13:01:24 +0900

    feat(프로그래머스): lv2 두 큐 합 같게 만들기

--- src/main/java/level2/Algo66.java
+++ src/main/java/level2/Algo66.java
@@ -1,9 +1,51 @@
 package level2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Algo66 {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = -2;
+        int answer = 0;
+
+        long sum1 = getSum(queue1);
+        long sum2 = getSum(queue2);
+
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        for(int i = 0; i < queue1.length; i++) {
+            q1.add(queue1[i]);
+            q2.add(queue2[i]);
+        }
+
+        while(sum1 != sum2) {
+            if(answer > (queue1.length + queue2.length) * 2){
+                return -1;
+            }
+            int val = 0;
+            if(sum1 < sum2) {
+                val = q2.poll();
+                q1.add(val);
+                sum1 += (long) val;
+                sum2 -= (long) val;
+            }
+            else {
+                val = q1.poll();
+                q2.add(val);
+                sum1 -= (long) val;
+                sum2 += (long) val;
+            }
+            answer++;
+        }
         return answer;
+    }
+
+    public long getSum(int[] q){
+        long sum = 0;
+        for (int i = 0; i < q.length; i++) {
+            sum += q[i];
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
