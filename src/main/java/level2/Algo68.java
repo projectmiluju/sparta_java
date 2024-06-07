31c63d46b1eeab645dd24acd2ede5ab5ff08a3d5
projectmiluju <dnjsdyd0712@naver.com>
2024-06-07 13:41:02 +0900

    feat(프로그래머스): lv2 무인도 여행

--- src/main/java/level2/Algo68.java
+++ src/main/java/level2/Algo68.java
@@ -1,11 +1,45 @@
 package level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Algo68 {
+    static int sum=0;
+
     public int[] solution(String[] maps) {
-        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        boolean[][] visited = new boolean[maps.length][maps[0].length()];
+        for(int i=0; i<maps.length; i++) {
+            for(int j=0; j<maps[i].length(); j++) {
+                dfs(maps, visited, i, j);
+                if(sum>0) {
+                    list.add(sum);
+                    sum=0;
+                }
+            }
+        }
+        if(list.isEmpty()) return new int[] {-1};
+
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i]=list.get(i);
+        }
+        Arrays.sort(answer);
         return answer;
+    }
+
+    public void dfs(String[] maps, boolean[][] visited, int i, int j) {
+        if(i<0 || j<0 || i>=maps.length || j>=maps[0].length()) return;
+        if(maps[i].charAt(j) == 'X' || visited[i][j]) return;
+        else {
+            visited[i][j]=true;
+            sum+=maps[i].charAt(j)-'0';
+        }
+        dfs(maps, visited, i+1, j);
+        dfs(maps, visited, i-1, j);
+        dfs(maps, visited, i, j+1);
+        dfs(maps, visited, i, j-1);
     }
 
     public static void main(String[] args) {
