39cbc7833745c8e60ccff4fa5dac4d5b1cf8b10c
projectmiluju <dnjsdyd0712@naver.com>
2024-06-12 20:15:29 +0900

    feat(프로그래머스): lv4 무지의 먹방 라이브

--- src/main/java/level4/Algo1.java
+++ src/main/java/level4/Algo1.java
@@ -1,9 +1,54 @@
 package level4;
 
+import java.util.*;
+
+class Food implements Comparable<Food> {
+    int time;
+    int idx;
+
+    public Food(int time, int idx) {
+        this.time = time;
+        this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(Food other) {
+        return Integer.compare(this.time, other.time);
+    }
+}
 public class Algo1 {
     public int solution(int[] food_times, long k) {
-        int answer = 0;
-        return answer;
+        long food_sum = 0;
+        for (int foodTime : food_times) {
+            food_sum += foodTime;
+        }
+
+        if (food_sum <= k) return -1;
+
+        PriorityQueue<Food> pq = new PriorityQueue<>();
+        for (int i = 0; i < food_times.length; i++) {
+            pq.offer(new Food(food_times[i], i + 1));
+        }
+
+        long total = 0;
+        long previous = 0;
+        long length = food_times.length;
+
+        while (total + ((Objects.requireNonNull(pq.peek()).time - previous) * length) <= k) {
+            int now = pq.poll().time;
+            total += (now - previous) * length;
+            length -= 1;
+            previous = now;
+        }
+
+        ArrayList<Food> result = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            result.add(pq.poll());
+        }
+
+        result.sort(Comparator.comparingInt(a -> a.idx));
+
+        return result.get((int) ((k - total) % length)).idx;
     }
 
     public static void main(String[] args) {
