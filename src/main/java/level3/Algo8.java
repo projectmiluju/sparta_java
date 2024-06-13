b13af364b4d2c8c310bb631568d421b6504c3eee
projectmiluju <dnjsdyd0712@naver.com>
2024-06-13 21:00:29 +0900

    feat(프로그래머스): lv3 베스트앨범

--- src/main/java/level3/Algo8.java
+++ src/main/java/level3/Algo8.java
@@ -1,11 +1,40 @@
 package level3;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Algo8 {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        return answer;
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        HashMap<String, Integer> num = new HashMap<>();
+        HashMap<String, HashMap<Integer, Integer>> music = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
+            if(!num.containsKey(genres[i])){
+                HashMap<Integer, Integer> map = new HashMap<>();
+                map.put(i,plays[i]);
+                music.put(genres[i], map);
+                num.put(genres[i], plays[i]);
+            }else {
+                music.get(genres[i]).put(i,plays[i]);
+                num.put(genres[i], num.get(genres[i]) + plays[i]);
+            }
+        }
+
+        List<String> keySet = new ArrayList<>(music.keySet());
+        keySet.sort((s1, s2) -> num.get(s2) - num.get(s1));
+
+        for (String key : keySet) {
+            HashMap<Integer, Integer> map = music.get(key);
+            List<Integer> list = new ArrayList<>(map.keySet());
+
+            list.sort((s1, s2) -> map.get(s2) - map.get(s1));
+
+            answer.add(list.get(0));
+            if (list.size() > 1) {
+                answer.add(list.get(1));
+            }
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 
     public static void main(String[] args) {
