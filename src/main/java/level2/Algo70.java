a4d5488cfc34ef283480c37da542de0ceba70041
projectmiluju <dnjsdyd0712@naver.com>
2024-06-29 23:39:01 +0900

    feat(프로그래머스): lv2 줄 서는 방법

--- src/main/java/level2/Algo70.java
+++ src/main/java/level2/Algo70.java
@@ -1,14 +1,35 @@
 package level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Algo70 {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
 
+        List<Integer> people = new ArrayList<>();
+        long factorial = 1;
+        int index = 0;
+
+        for(int i=1; i<=n; i++) {
+            factorial *= i;
+            people.add(i);
+        }
+
+        k--;
+        while(n > 0) {
+            factorial /= n;
+            int value = (int) (k / factorial);
+            answer[index++] = people.get(value);
+            people.remove(value);
+
+            k %= factorial;
+            n--;
+        }
+
         return answer;
     }
-
     public static void main(String[] args) {
         Algo70 algo70 = new Algo70();
 
