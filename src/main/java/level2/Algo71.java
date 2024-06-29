74a90f37d22c5982cf5b58a0da37fad9cd027c69
projectmiluju <dnjsdyd0712@naver.com>
2024-06-30 00:02:52 +0900

    feat(프로그래머스): lv2 모음사전

--- src/main/java/level2/Algo71.java
+++ src/main/java/level2/Algo71.java
@@ -1,10 +1,32 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Algo71 {
+    static List<String> list;
+    static String [] words = {"A", "E", "I", "O", "U"};
     public int solution(String word) {
         int answer = 0;
+        list = new ArrayList<>();
+        dfs("", 0);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
+        }
         return answer;
     }
+
+    static void dfs(String str, int len) {
+        list.add(str);
+        if (len == 5) return;
+        for (int i = 0; i < 5; i++) {
+            dfs(str + words[i], len + 1);
+        }
+    }
+
 
     public static void main(String[] args) {
         Algo71 algo71 = new Algo71();
