253d9217da24d3486b7314172c61d5695f7859a6
projectmiluju <dnjsdyd0712@naver.com>
2024-07-01 09:54:25 +0900

    feat(프로그래머스): lv2 아날로그 시계

--- src/main/java/level2/Algo72.java
+++ src/main/java/level2/Algo72.java
@@ -2,8 +2,27 @@ package level2;
 
 public class Algo72 {
     public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
-        int answer = -1;
-        return answer;
+
+        int start = toSecond(h1,m1,s1);
+        int end = toSecond(h2,m2,s2);
+
+        return cal(end) - cal(start) + (alarmNow(start)? 1:0);
+    }
+    static int cal(int time){
+        int sm = time*59/3600;
+
+        int sh = time*719/43200;
+
+        int a = 43200 <= time ? 2 : 1;
+
+        return sm+sh - a;
+    }
+
+    static int toSecond(int h,int m,int s){
+        return h*3600+m*60+s;
+    }
+    static boolean alarmNow(int time){
+        return time*59%3600==0 || time*719%43200==0;
     }
 
     public static void main(String[] args) {
