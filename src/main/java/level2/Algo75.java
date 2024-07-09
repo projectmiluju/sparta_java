0eb0bdf51c79c0b1aa3de6022e90c220323ed65b
projectmiluju <dnjsdyd0712@naver.com>
2024-07-10 02:59:46 +0900

    feat(프로그래머스): lv2 가장 큰 정사각형 찾기

--- src/main/java/level2/Algo75.java
+++ src/main/java/level2/Algo75.java
@@ -2,8 +2,21 @@ package level2;
 
 public class Algo75 {
     public int solution(int[][] board) {
-        int answer = 1234;
-        return answer;
+        int[][] map = new int[board.length + 1][board[0].length + 1];
+
+        int maxLen = 0;
+        for (int i = 1; i <= board.length; i++) {
+            for (int j = 1; j <= board[0].length; j++) {
+                if(board[i-1][j-1] != 0) {
+                    int min = Math.min(Math.min(map[i - 1][j], map[i][j - 1]), map[i - 1][j - 1]);
+                    map[i][j] = min + 1;
+
+                    maxLen = Math.max(maxLen, min + 1);
+                }
+            }
+        }
+
+        return maxLen * maxLen;
     }
 
     public static void main(String[] args) {
