2991300e2041cbac206d6d0dc8b89645c9e398df
projectmiluju <dnjsdyd0712@naver.com>
2024-07-11 03:12:00 +0900

    feat(프로그래머스): lv2 하노이의 탑

--- src/main/java/level2/Algo76.java
+++ src/main/java/level2/Algo76.java
@@ -1,11 +1,22 @@
 package level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Algo76 {
+    private static final List<int[]> arr = new ArrayList<>();
+
     public int[][] solution(int n) {
-        int[][] answer = {};
-        return answer;
+        move(n, 1, 2, 3);
+        return arr.toArray(int[][]::new);
+    }
+
+    private static void move(int cnt, int start, int mid, int end) {
+        if (cnt == 0) return;
+        move(cnt - 1, start, end, mid);
+        arr.add(new int[]{start, end});
+        move(cnt - 1, mid, start, end);
     }
 
     public static void main(String[] args) {
