461cf84e9518c954cd0b1616507be4e707373af4
projectmiluju <dnjsdyd0712@naver.com>
2024-07-12 16:44:00 +0900

    feat(프로그래머스): lv2 디펜스 게임

--- src/main/java/level2/Algo77.java
+++ src/main/java/level2/Algo77.java
@@ -1,9 +1,32 @@
 package level2;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Algo77 {
 
     public int solution(int n, int k, int[] enemy) {
-        int answer = 0;
+        int answer = enemy.length;
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int my = n;
+        int card = k;
+        for (int i = 0; i < enemy.length; i++) {
+            my -= enemy[i];
+            pq.add(enemy[i]);
+
+            if (my < 0) {
+                if (card > 0 && !pq.isEmpty()) {
+                    my += pq.poll();
+                    card--;
+                } else {
+                    answer = i;
+                    break;
+                }
+            }
+        }
+
         return answer;
     }
 
