ef731b417e6e7e8d7fab8cce9e1b64f0a41465e1
projectmiluju <dnjsdyd0712@naver.com>
2024-07-17 10:00:30 +0900

    feat(프로그래머스): lv2 피로도

--- src/main/java/level2/Algo79.java
+++ src/main/java/level2/Algo79.java
@@ -1,9 +1,25 @@
 package level2;
 
 public class Algo79 {
+    static boolean[] visited;
+    static int count;
+
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        return answer;
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
+        return count;
+    }
+
+    private void dfs(int depth, int fatigue, int[][] dungeons){
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i] || dungeons[i][0] > fatigue) {
+                continue;
+            }
+            visited[i] = true;
+            dfs(depth + 1, fatigue - dungeons[i][1], dungeons);
+            visited[i] = false;
+        }
+        count = Math.max(count, depth);
     }
 
     public static void main(String[] args) {
