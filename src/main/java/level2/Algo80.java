744bbdb94c9bb023ad942a89b711f134c1606e0e
projectmiluju <dnjsdyd0712@naver.com>
2024-07-19 08:01:17 +0900

    feat(프로그래머스): lv2 점 찍기

---
+++ src/main/java/level2/Algo80.java
@@ -0,0 +1,23 @@
+package level2;
+
+public class Algo80 {
+    public long solution(int k, int d) {
+        long answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algo80 algo = new Algo80();
+
+        int k1 = 2, d1 = 4, res1 = 6;
+        long ans1 = algo.solution(k1, d1);
+        System.out.println(ans1);
+        if (res1 == ans1) System.out.println("Y"); else System.out.println("N");
+
+        int k2 = 1, d2 = 5, res2 = 28;
+        long ans2 = algo.solution(k2, d2);
+        System.out.println(ans2);
+        if (res2 == ans2) System.out.println("Y"); else System.out.println("N");
+    }
+
+}
