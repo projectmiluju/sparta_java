82da941610bc16c0df2e7304957e57bc6b9f126f
projectmiluju <dnjsdyd0712@naver.com>
2025-03-14 02:18:39 +0900

    feat(구름): 위클리 챌린지1_3

---
+++ src/main/java/groom/weekly1_3.java
@@ -0,0 +1,12 @@
+package groom;
+
+import java.io.*;
+
+public class weekly1_3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        System.out.println();
+    }
+}
