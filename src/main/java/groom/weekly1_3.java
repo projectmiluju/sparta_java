2be33475f5133ef37208ac7b0deff64eaa41461b
projectmiluju <dnjsdyd0712@naver.com>
2025-03-14 04:14:25 +0900

    feat(구름): 위클리 챌린지1_3
    int와 long 잘 사용할것

--- src/main/java/groom/weekly1_3.java
+++ src/main/java/groom/weekly1_3.java
@@ -14,55 +14,42 @@ public class weekly1_3 {
             strings.add(br.readLine());
         }
 
-        int n = Integer.parseInt(strings.get(0).split(" ")[0]);
-        int m = Integer.parseInt(strings.get(0).split(" ")[1]);
-        int x = Integer.parseInt(strings.get(0).split(" ")[2]);
+        String[] firstLine = strings.get(0).split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        int m = Integer.parseInt(firstLine[1]);
+        int x = Integer.parseInt(firstLine[2]);
+
         int q = Integer.parseInt(strings.get(2));
+
 
         int[] arr = Arrays.stream(strings.get(1).split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        int answer = 0;
-        if (arr[x-1] >= m) {
-            answer += arr[x-1];
-            arr[x-1] = 0;
+        Long answer = 0L;
+        if (arr[x - 1] >= m) {
+            answer += arr[x - 1];
+            arr[x - 1] = 0;
         }
-        Arrays.setAll(arr, j -> arr[j] + 1);
+        for (int i = 0; i < n; i++) arr[i]++;
 
-        String[] arr2 = strings.get(3).split(" ");
+        String[] commands = strings.get(3).split(" ");
 
-        for (int i = 0; i < q-1; i++) {
-            switch (arr2[i]) {
-                case "L" -> {
-                    if (x == 1) {
-                        x = n;
-                    } else {
-                        x = x - 1;
-                    }
-                    if (arr[x - 1] >= m) {
-                        answer += arr[x - 1];
-                        arr[x - 1] = 0;
-                    }
-                }
-                case "R" -> {
-                    if (x == n) {
-                        x = 1;
-                    } else {
-                        x = x + 1;
-                    }
-                    if (arr[x - 1] >= m) {
-                        answer += arr[x - 1];
-                        arr[x - 1] = 0;
-                    }
-                }
-                case "S" -> {
-                    if (arr[x - 1] >= m) {
-                        answer += arr[x - 1];
-                        arr[x - 1] = 0;
-                    }
-                }
+        for (int i = 0; i < q - 1; i++) {
+            char cmd = commands[i].charAt(0);
+
+            if (cmd == 'L') {
+                x = (x == 1) ? n : x - 1;
+            } else if (cmd == 'R') {
+                x = (x == n) ? 1 : x + 1;
             }
-            Arrays.setAll(arr, j -> arr[j] + 1);
+
+            if (arr[x - 1] >= m) {
+                answer += arr[x - 1];
+                arr[x - 1] = 0;
+            }
+
+            for (int j = 0; j < n; j++) arr[j]++;
         }
+
         System.out.println(answer);
     }
 }
