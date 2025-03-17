967267c4f055c36dc2a418e00ea2f3441ea2bf46
projectmiluju <dnjsdyd0712@naver.com>
2025-03-18 06:14:47 +0900

    feat(구름): 알고리즘5_1
    나무꾼 구름이

--- src/main/java/groom/mission/A5_1.java
+++ src/main/java/groom/mission/A5_1.java
@@ -2,11 +2,55 @@ package groom.mission;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class A5_1 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        System.out.println("Hello Goorm! Your input is " + input);
+        List<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            strings.add(br.readLine());
+        }
+
+        String[] firstLine = strings.get(0).split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        int m = Integer.parseInt(firstLine[1]);
+        int x = Integer.parseInt(firstLine[2]);
+
+        int q = Integer.parseInt(strings.get(2));
+
+
+        int[] arr = Arrays.stream(strings.get(1).split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Long answer = 0L;
+        if (arr[x - 1] >= m) {
+            answer += arr[x - 1];
+            arr[x - 1] = 0;
+        }
+        for (int i = 0; i < n; i++) arr[i]++;
+
+        String[] commands = strings.get(3).split(" ");
+
+        for (int i = 0; i < q - 1; i++) {
+            char cmd = commands[i].charAt(0);
+
+            if (cmd == 'L') {
+                x = (x == 1) ? n : x - 1;
+            } else if (cmd == 'R') {
+                x = (x == n) ? 1 : x + 1;
+            }
+
+            if (arr[x - 1] >= m) {
+                answer += arr[x - 1];
+                arr[x - 1] = 0;
+            }
+
+            for (int j = 0; j < n; j++) arr[j]++;
+        }
+
+        System.out.println(answer);
     }
 }
