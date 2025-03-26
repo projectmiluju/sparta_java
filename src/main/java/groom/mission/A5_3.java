fdf3605f779dc9741f9af3d35f10078f36f205a4
projectmiluju <dnjsdyd0712@naver.com>
2025-03-27 00:55:54 +0900

    feat(구름): 알고리즘5_3
    빵야

--- src/main/java/groom/mission/A5_3.java
+++ src/main/java/groom/mission/A5_3.java
@@ -2,21 +2,50 @@ package groom.mission;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class A5_3 {
+
+    static final int MAX = 100000;
+    static int N;
+    static int[] H = new int[MAX];
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        long cnt = 0;
-        for (int i = 0; i <N; i++) {
-            long h = Integer.parseInt(input[i]);
-            while(h > 0){
-                cnt++;
-                h = h - ((cnt - 1) % 4 + 1);
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            H[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long answer = 0;
+        int ct = 0;
+
+        for (int i = 0; i < N; i++) {
+
+            while (ct > 0 && H[i] > 0) {
+                H[i] -= ct + 1;
+                ct = (ct + 1) % 4;
+                answer++;
+            }
+
+            if (H[i] <= 0) {
+                continue;
+            }
+
+            answer += (H[i] / 10) * 4;
+            H[i] %= 10;
+
+            while (H[i] > 0) {
+                H[i] -= ct + 1;
+                ct = (ct + 1) % 4;
+                answer++;
             }
         }
-        System.out.println(cnt);
+
+        System.out.println(answer);
     }
 }
