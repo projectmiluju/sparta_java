70ea7133e5e708b73294a3ddebbb64f84f0ba3cc
projectmiluju <dnjsdyd0712@naver.com>
2025-03-30 10:48:39 +0900

    feat(구름): 알고리즘6_3
    GameJam

---
+++ src/main/java/groom/mission/A6_3.java
@@ -0,0 +1,11 @@
+package groom.mission;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class A6_3 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
