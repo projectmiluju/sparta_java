c2d41c4a88979c9b9b6b44e7aaa3a92f32252c9d
projectmiluju <dnjsdyd0712@naver.com>
2025-04-04 04:50:30 +0900

    feat(구름): 알고리즘11_1 자료구조 뒤통수가 따가워

--- src/main/java/groom/mission/A11_1.java
+++ src/main/java/groom/mission/A11_1.java
@@ -1,12 +1,13 @@
 package groom.mission;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 // 뒤통수가 따가워
 public class A11_1 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
