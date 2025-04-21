360e892b5ea1bc568ba3cf0a1dfcb098523cc542
projectmiluju <dnjsdyd0712@naver.com>
2025-04-22 06:27:52 +0900

    feat(구름): 알고리즘12_1 그래프 이론 폭탄해제

--- src/main/java/groom/mission/A12_1.java
+++ src/main/java/groom/mission/A12_1.java
@@ -6,7 +6,15 @@ import java.io.InputStreamReader;
 public class A12_1 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        System.out.println("Hello Goorm! Your input is " + input);
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        for (int i = 0; i < m; i++) {
+            input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+        }
     }
 }
