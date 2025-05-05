82cc3df7b8c47981d8273fa89f961433c459d1a2
projectmiluju <dnjsdyd0712@naver.com>
2025-05-05 18:47:29 +0900

    feat(구름): 알고리즘12_1 그래프 이론 폭탄해제

--- src/main/java/groom/mission/A12_1.java
+++ src/main/java/groom/mission/A12_1.java
@@ -1,21 +1,55 @@
 package groom.mission;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class A12_1 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
+    static int n, m;
+    static int[][] wires;
+    static int[] connectionCount;
 
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        wires = new int[m][2];
+        connectionCount = new int[n + 1];
 
         for (int i = 0; i < m; i++) {
-            input = br.readLine().split(" ");
-            int a = Integer.parseInt(input[0]);
-            int b = Integer.parseInt(input[1]);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            wires[i][0] = a;
+            wires[i][1] = b;
+            connectionCount[a]++;
+            connectionCount[b]++;
+        }
+
+        List<Integer> safeWires = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            int a = wires[i][0];
+            int b = wires[i][1];
+
+            // 해당 전선만 끊는 시뮬레이션
+            connectionCount[a]--;
+            connectionCount[b]--;
+
+            if (connectionCount[a] > 0 && connectionCount[b] > 0) {
+                safeWires.add(i + 1);
+            }
+
+            connectionCount[a]++;
+            connectionCount[b]++;
+        }
+
+        if (safeWires.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            for (int wire : safeWires) {
+                System.out.print(wire + " ");
+            }
         }
     }
-
 }
