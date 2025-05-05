15369c6ddd2dfffa08bfe7e07a2ebbfbf431aaad
projectmiluju <dnjsdyd0712@naver.com>
2025-05-05 23:04:46 +0900

    feat(구름): 알고리즘12_2 그래프 이론 가뭄

---
+++ src/main/java/groom/mission/A12_2.java
@@ -0,0 +1,12 @@
+package groom.mission;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class A12_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println("Hello Goorm! Your input is " + input);
+    }
+}
