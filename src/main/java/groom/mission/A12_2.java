2734a419832d83c444d11ac7f7714ad133a7c844
projectmiluju <dnjsdyd0712@naver.com>
2025-05-05 23:09:51 +0900

    feat(구름): 알고리즘12_2 그래프 이론 가뭄
    일부 케이스 타임아웃

--- src/main/java/groom/mission/A12_2.java
+++ src/main/java/groom/mission/A12_2.java
@@ -1,12 +1,57 @@
 package groom.mission;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class A12_2 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        System.out.println("Hello Goorm! Your input is " + input);
+    static int[] parent;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t --> 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            int[][] edges = new int[m][2];
+
+            for (int i = 0; i < m; i++) {
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
+            }
+
+            parent = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                parent[i] = i;
+            }
+
+            int edgeCount = 0;
+
+            for (int i = 0; i < m; i++) {
+                int a = edges[i][0];
+                int b = edges[i][1];
+                if (union(a, b)) {
+                    edgeCount++;
+                    if (edgeCount == n - 1) break;
+                }
+            }
+
+            System.out.println(edgeCount);
+        }
+    }
+
+    static int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    static boolean union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA == rootB) return false;
+        parent[rootB] = rootA;
+        return true;
     }
 }
+
