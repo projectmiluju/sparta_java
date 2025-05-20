4aed433835cae7e0f273b3eb00da73032fbf2612
projectmiluju <dnjsdyd0712@naver.com>
2025-05-21 04:22:04 +0900

    feat(구름): 알고리즘14_2 미사일 발사

--- src/main/java/groom/mission/A14_2.java
+++ src/main/java/groom/mission/A14_2.java
@@ -6,7 +6,20 @@ import java.io.InputStreamReader;
 public class A14_2 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        System.out.println("Hello Goorm! Your input is " + input);
+
+        // 미사일 개수
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            // 외계인 x 좌표
+            int x = Integer.parseInt(input[0]);
+            // 외계인 y 좌표
+            int y = Integer.parseInt(input[1]);
+            // 미사일 발표 시각
+            int t = Integer.parseInt(input[2]);
+
+        }
+
     }
 }
