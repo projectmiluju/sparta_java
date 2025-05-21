92d329787a136b53181621bea67bc8cbe7c9a4b1
projectmiluju <dnjsdyd0712@naver.com>
2025-05-22 05:02:47 +0900

    feat(구름): 알고리즘14_3 초코 쿠키

--- src/main/java/groom/mission/A14_3.java
+++ src/main/java/groom/mission/A14_3.java
@@ -3,6 +3,8 @@ package groom.mission;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class A14_3 {
     public static void main(String[] args) throws Exception {
@@ -11,11 +13,30 @@ public class A14_3 {
         // 초코쿠키 개수
         int n = Integer.parseInt(input[0]);
         // 초코쿠키 마다 맛있는 정도 수치
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] arr = new int[n+1];
         input = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(input[i]));
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(input[i-1]);
         }
 
+        List<int[]> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(new int[]{arr[i], i});
+        }
+
+        list.sort(Comparator.comparingInt(o -> o[0]));
+
+        for (int i = 0; i < n; i++) {
+            if (list.get(i)[0] - i <= 0) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print(j + " ");
+                }
+              return;
+            }
+        }
+
+        for (int[] pair : list) {
+            System.out.print(pair[1] + " ");
+        }
     }
 }
