12ebc07e70d21bc44bc2080f6b6c54932817216d
projectmiluju <dnjsdyd0712@naver.com>
2025-05-22 05:15:07 +0900

    feat(구름): 알고리즘14_3 초코 쿠키

--- src/main/java/groom/mission/A14_2.java
+++ src/main/java/groom/mission/A14_2.java
@@ -5,19 +5,25 @@ import java.io.InputStreamReader;
 
 public class A14_2 {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int MAX = 100000;
+        long[] x = new long[MAX];
+        long[] y = new long[MAX];
+        long[] l = new long[MAX];
+        long[] r = new long[MAX];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 미사일 개수
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
             String[] input = br.readLine().split(" ");
             // 외계인 x 좌표
-            int x = Integer.parseInt(input[0]);
+            x[i] = Integer.parseInt(input[0]);
             // 외계인 y 좌표
-            int y = Integer.parseInt(input[1]);
+            y[i] = Integer.parseInt(input[1]);
             // 미사일 발표 시각
-            int t = Integer.parseInt(input[2]);
+            l[i] = Integer.parseInt(input[2]);
 
         }
 
