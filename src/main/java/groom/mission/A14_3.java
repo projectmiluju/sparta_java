e10a9d2068f6e932f955ffa71921547b2fe8c332
projectmiluju <dnjsdyd0712@naver.com>
2025-05-22 04:54:29 +0900

    feat(구름): 알고리즘14_3 초코 쿠키

--- src/main/java/groom/mission/A14_3.java
+++ src/main/java/groom/mission/A14_3.java
@@ -2,11 +2,20 @@ package groom.mission;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class A14_3 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        System.out.println("Hello Goorm! Your input is " + input);
+        String[] input = br.readLine().split(" ");
+        // 초코쿠키 개수
+        int n = Integer.parseInt(input[0]);
+        // 초코쿠키 마다 맛있는 정도 수치
+        ArrayList<Integer> list = new ArrayList<>();
+        input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(input[i]));
+        }
+
     }
 }
