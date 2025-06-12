a95612c560d2bc7ce2cb9ba173b1d6ae30d0d242
projectmiluju <dnjsdyd0712@naver.com>
2025-06-12 18:08:30 +0900

    feat(구름): 동적프로그래밍 15_1 피보나치 수열

--- src/main/java/groom/mission/A15_1.java
+++ src/main/java/groom/mission/A15_1.java
@@ -1,12 +1,30 @@
 package groom.mission;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class A15_1 {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        System.out.println("Hello World! Your input is " + input);
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        final int MOD = 1_000_000_007;
+
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        } else if (N == 2) {
+            System.out.println(1);
+            return;
+        }
+
+        long a = 0;
+        long b = 1;
+        long c = 0;
+        for (int i = 3; i <= N; i++) {
+            c = (a + b) % MOD;
+            a = b;
+            b = c;
+        }
+
+        System.out.println(c);
     }
 }
